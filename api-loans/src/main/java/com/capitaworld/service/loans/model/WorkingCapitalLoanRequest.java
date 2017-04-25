3e65d0603ff8402ae3ce1a2c6f7c3bde753fe368
AkshayCW <akshay.patel@capitaworld.com>
2017-04-25 10:25:39 +0530

    WorkingCapitalLoanRequest model added

---
+++ api-loans/src/main/java/com/capitaworld/service/loans/model/WorkingCapitalLoanRequest.java
@@ -0,0 +1,246 @@
+package com.capitaworld.service.loans.model;
+
+import java.io.Serializable;
+
+public class WorkingCapitalLoanRequest implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long applicationId;
+	private Integer accountingSystemsId;
+	private Integer brandAmbassadorId;
+	private Integer competenceId;
+	private Long creditRatingId;
+	private Integer currencyId;
+	private Integer denominationId;
+	private String descriptionEnhancementLimit;
+	private Integer distributionAndMarketingTieUpsId;
+	private Integer environmentCertificationId;
+	private Integer existingShareHoldersId;
+	private String haveExistingLimit;
+	private Integer indiaDistributionNetworkId;
+	private Integer integerernalAuditId;
+	private Boolean isDependsMajorlyOnGovernment;
+	private Boolean isIsoCertified;
+	private Integer marketPositionId;
+	private Integer marketPositioningTopId;
+	private Integer marketShareTurnoverId;
+	private Integer marketingPositioningId;
+	private Integer productServicesPerseId;
+	private String projectBrief;
+	private Integer technologyPatentedId;
+	private Integer technologyRequiresUpgradationId;
+	private Boolean typeOfTechnologyCorporateId;
+	private Boolean is_technology_tied;
+
+	public WorkingCapitalLoanRequest() {
+	}
+
+	public Long getApplicationId() {
+		return applicationId;
+	}
+
+	public void setApplicationId(Long applicationId) {
+		this.applicationId = applicationId;
+	}
+
+	public Integer getAccountingSystemsId() {
+		return accountingSystemsId;
+	}
+
+	public void setAccountingSystemsId(Integer accountingSystemsId) {
+		this.accountingSystemsId = accountingSystemsId;
+	}
+
+	public Integer getBrandAmbassadorId() {
+		return brandAmbassadorId;
+	}
+
+	public void setBrandAmbassadorId(Integer brandAmbassadorId) {
+		this.brandAmbassadorId = brandAmbassadorId;
+	}
+
+	public Integer getCompetenceId() {
+		return competenceId;
+	}
+
+	public void setCompetenceId(Integer competenceId) {
+		this.competenceId = competenceId;
+	}
+
+	public Long getCreditRatingId() {
+		return creditRatingId;
+	}
+
+	public void setCreditRatingId(Long creditRatingId) {
+		this.creditRatingId = creditRatingId;
+	}
+
+	public Integer getCurrencyId() {
+		return currencyId;
+	}
+
+	public void setCurrencyId(Integer currencyId) {
+		this.currencyId = currencyId;
+	}
+
+	public Integer getDenominationId() {
+		return denominationId;
+	}
+
+	public void setDenominationId(Integer denominationId) {
+		this.denominationId = denominationId;
+	}
+
+	public String getDescriptionEnhancementLimit() {
+		return descriptionEnhancementLimit;
+	}
+
+	public void setDescriptionEnhancementLimit(String descriptionEnhancementLimit) {
+		this.descriptionEnhancementLimit = descriptionEnhancementLimit;
+	}
+
+	public Integer getDistributionAndMarketingTieUpsId() {
+		return distributionAndMarketingTieUpsId;
+	}
+
+	public void setDistributionAndMarketingTieUpsId(Integer distributionAndMarketingTieUpsId) {
+		this.distributionAndMarketingTieUpsId = distributionAndMarketingTieUpsId;
+	}
+
+	public Integer getEnvironmentCertificationId() {
+		return environmentCertificationId;
+	}
+
+	public void setEnvironmentCertificationId(Integer environmentCertificationId) {
+		this.environmentCertificationId = environmentCertificationId;
+	}
+
+	public Integer getExistingShareHoldersId() {
+		return existingShareHoldersId;
+	}
+
+	public void setExistingShareHoldersId(Integer existingShareHoldersId) {
+		this.existingShareHoldersId = existingShareHoldersId;
+	}
+
+	public String getHaveExistingLimit() {
+		return haveExistingLimit;
+	}
+
+	public void setHaveExistingLimit(String haveExistingLimit) {
+		this.haveExistingLimit = haveExistingLimit;
+	}
+
+	public Integer getIndiaDistributionNetworkId() {
+		return indiaDistributionNetworkId;
+	}
+
+	public void setIndiaDistributionNetworkId(Integer indiaDistributionNetworkId) {
+		this.indiaDistributionNetworkId = indiaDistributionNetworkId;
+	}
+
+	public Integer getIntegerernalAuditId() {
+		return integerernalAuditId;
+	}
+
+	public void setIntegerernalAuditId(Integer integerernalAuditId) {
+		this.integerernalAuditId = integerernalAuditId;
+	}
+
+	public Boolean getIsDependsMajorlyOnGovernment() {
+		return isDependsMajorlyOnGovernment;
+	}
+
+	public void setIsDependsMajorlyOnGovernment(Boolean isDependsMajorlyOnGovernment) {
+		this.isDependsMajorlyOnGovernment = isDependsMajorlyOnGovernment;
+	}
+
+	public Boolean getIsIsoCertified() {
+		return isIsoCertified;
+	}
+
+	public void setIsIsoCertified(Boolean isIsoCertified) {
+		this.isIsoCertified = isIsoCertified;
+	}
+
+	public Integer getMarketPositionId() {
+		return marketPositionId;
+	}
+
+	public void setMarketPositionId(Integer marketPositionId) {
+		this.marketPositionId = marketPositionId;
+	}
+
+	public Integer getMarketPositioningTopId() {
+		return marketPositioningTopId;
+	}
+
+	public void setMarketPositioningTopId(Integer marketPositioningTopId) {
+		this.marketPositioningTopId = marketPositioningTopId;
+	}
+
+	public Integer getMarketShareTurnoverId() {
+		return marketShareTurnoverId;
+	}
+
+	public void setMarketShareTurnoverId(Integer marketShareTurnoverId) {
+		this.marketShareTurnoverId = marketShareTurnoverId;
+	}
+
+	public Integer getMarketingPositioningId() {
+		return marketingPositioningId;
+	}
+
+	public void setMarketingPositioningId(Integer marketingPositioningId) {
+		this.marketingPositioningId = marketingPositioningId;
+	}
+
+	public Integer getProductServicesPerseId() {
+		return productServicesPerseId;
+	}
+
+	public void setProductServicesPerseId(Integer productServicesPerseId) {
+		this.productServicesPerseId = productServicesPerseId;
+	}
+
+	public String getProjectBrief() {
+		return projectBrief;
+	}
+
+	public void setProjectBrief(String projectBrief) {
+		this.projectBrief = projectBrief;
+	}
+
+	public Integer getTechnologyPatentedId() {
+		return technologyPatentedId;
+	}
+
+	public void setTechnologyPatentedId(Integer technologyPatentedId) {
+		this.technologyPatentedId = technologyPatentedId;
+	}
+
+	public Integer getTechnologyRequiresUpgradationId() {
+		return technologyRequiresUpgradationId;
+	}
+
+	public void setTechnologyRequiresUpgradationId(Integer technologyRequiresUpgradationId) {
+		this.technologyRequiresUpgradationId = technologyRequiresUpgradationId;
+	}
+
+	public Boolean getTypeOfTechnologyCorporateId() {
+		return typeOfTechnologyCorporateId;
+	}
+
+	public void setTypeOfTechnologyCorporateId(Boolean typeOfTechnologyCorporateId) {
+		this.typeOfTechnologyCorporateId = typeOfTechnologyCorporateId;
+	}
+
+	public Boolean getIs_technology_tied() {
+		return is_technology_tied;
+	}
+
+	public void setIs_technology_tied(Boolean is_technology_tied) {
+		this.is_technology_tied = is_technology_tied;
+	}
+
+}
