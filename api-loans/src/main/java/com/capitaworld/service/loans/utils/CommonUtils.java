604be511d932d4de850d77a3fd037d83961e933b
AkshayCW <akshay.patel@capitaworld.com>
2017-04-29 13:30:55 +0530

    Added LoanType ENUM

--- api-loans/src/main/java/com/capitaworld/service/loans/utils/CommonUtils.java
+++ api-loans/src/main/java/com/capitaworld/service/loans/utils/CommonUtils.java
@@ -13,12 +13,45 @@ public class CommonUtils {
 				|| "".equals(((String) value).trim()) || "null".equals(value) || "undefined".equals(value)) : false));
 	}
 
-	public interface LoanType {
-		public static final int CORPORATE = 1;
-		public static final int RETAIL = 2;
+	public enum LoanType {
+		WORKING_CAPITAL(1), TERM_LOAN(2), HOME_LOAN(3),
+		CAR_LOAN(4);
+		private int value;
+
+		private LoanType(int value) {
+			this.value = value;
+		}
+
+		public int getValue() {
+			return value;
+		}
 	}
 
+	// // start Loan Type Common Variable
+	// public interface LoanType {
+	// public interface Corporate {
+	// public static final String CORPORATE_TYPE = "cr";
+	//
+	// public interface Debt {
+	// public static final String DEBT_TYPE = "dt";
+	// public static final String WORKING_CAPITAL = "wc";
+	// public static final String TERM_LOAN = "tl";
+	// }
+	// }
+	//
+	// public interface Retail {
+	// public static final String RETAIL_TYPE = "rt";
+	// public static final String HOME_LOAN = "hl";
+	// public static final String CAR_LOAN = "cl";
+	// public static final String EDUCATION_LOAN = "el";
+	// public static final String PERSONAL_LOAN = "pl";
+	// public static final String LOAN_AGAINST_PROPERTY = "lap";
+	// public static final String LOAN_AGAINST_SHARES = "las";
+	// }
+	// }
+
 	public interface IgnorableCopy {
-		public static final String[] CORPORATE = { "categoryCode", "productId", "name", "tenure", "userId" };
+		public static final String[] CORPORATE = { "categoryCode", "productId", "name", "tenure", "userId",
+				"isActive" };
 	}
 }
