67bda463d2428b213ee3b7cd66bf531565241837
AkshayCW <akshay.patel@capitaworld.com>
2017-05-01 14:13:56 +0530

    Added new Request Object named CoApplicant and Guarantor

---
+++ api-loans/src/main/java/com/capitaworld/service/loans/model/GuarantorRequest.java
@@ -0,0 +1,24 @@
+package com.capitaworld.service.loans.model;
+
+public class GuarantorRequest {
+
+	private Long id;
+	private String firstName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+}
--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/retail/Temp.java
+++
@@ -1,5 +0,0 @@
-package com.capitaworld.service.loans.service.fundseeker.retail;
-
-public class Temp {
-
-}
