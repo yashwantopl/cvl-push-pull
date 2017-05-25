634efccdac46c040e5b8ebc5d30d9c28db380657
jitesh <jitesh@jitesh-PC.CAPITAWORLD.COM>
2017-05-25 19:28:50 +0530

    retail parameter code change make response for credit rating for long term and short term credit rating option

--- service-loans/src/main/java/com/capitaworld/service/loans/controller/fundprovider/PersonalLoanParameterController.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/controller/fundprovider/PersonalLoanParameterController.java
@@ -1,5 +1,7 @@
 package com.capitaworld.service.loans.controller.fundprovider;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,7 +34,7 @@ public class PersonalLoanParameterController {
 	}
 
 	@RequestMapping(value = "/save", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<LoansResponse> save(@RequestBody PersonalLoanParameterRequest  personalLoanParameterRequest) {
+	public ResponseEntity<LoansResponse> save(@RequestBody PersonalLoanParameterRequest  personalLoanParameterRequest,HttpServletRequest request) {
 		// request must not be null
 		if (personalLoanParameterRequest == null) {
 			logger.warn("personalLoanParameterRequest Object can not be empty ==>", personalLoanParameterRequest);
@@ -49,13 +51,16 @@ public class PersonalLoanParameterController {
 					HttpStatus.OK);
 		}
 		
-		if(personalLoanParameterRequest.getId()==null)
+		Long userId = (Long) request.getAttribute(CommonUtils.USER_ID);
+		//Long userId=1755l;
+		if(userId==null)
 		{
-			logger.warn("user id can not be empty ==>", personalLoanParameterRequest);
+			logger.warn("userId  id can not be empty ==>", userId);
 			return new ResponseEntity<LoansResponse>(
 					new LoansResponse("Requested data can not be empty.", HttpStatus.BAD_REQUEST.value()),
 					HttpStatus.OK);
 		}
+		personalLoanParameterRequest.setUserId(userId);
 		
 		boolean response = personalLoanParameterService.saveOrUpdate(personalLoanParameterRequest);
 		if (response) {
