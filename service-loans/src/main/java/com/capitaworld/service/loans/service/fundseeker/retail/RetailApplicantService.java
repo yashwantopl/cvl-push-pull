7a7f2d8fc8180cf7554654417c9c445f3aa701ed
Dhaval <dhaval@capitaworld.com>
2017-05-26 10:57:21 +0530

    Method Removed

--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/retail/RetailApplicantService.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/retail/RetailApplicantService.java
@@ -19,4 +19,5 @@ public interface RetailApplicantService {
 	public List<CoApplicantRequest> getCoApplicants(Long userId, Long applicationId) throws Exception;
 	
 	public List<GuarantorRequest> getGuarantors(Long userId, Long applicationId) throws Exception;
+
 }
