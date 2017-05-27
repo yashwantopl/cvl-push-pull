3a13c593c2dea0ed11f0f6e6af2a5caec3a46dbc
Dhaval <dhaval@capitaworld.com>
2017-05-28 00:36:54 +0530

    Remove comment

--- service-loans/src/main/java/com/capitaworld/service/loans/controller/teaser/finalview/WorkingCapitalFinalViewController.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/controller/teaser/finalview/WorkingCapitalFinalViewController.java
@@ -32,7 +32,7 @@ public class WorkingCapitalFinalViewController {
 
         LoansResponse loansResponse = new LoansResponse();
         //get user id from http servlet request
-        Long userId = 1759l;/*(Long) httpServletRequest.getAttribute(CommonUtils.USER_ID);*/
+        Long userId = (Long) httpServletRequest.getAttribute(CommonUtils.USER_ID);
         if(CommonUtils.isObjectNullOrEmpty(toApplicationId)){
             logger.warn("Invalid data or Requested data not found.", toApplicationId);
             return new ResponseEntity<LoansResponse>(new LoansResponse("Invalid data or Requested data not found.", HttpStatus.BAD_REQUEST.value()),HttpStatus.OK);
