d089c8265a3717fa7d1f385f12e3d696988f3453
KushalCW <kushal@capitaworld.com>
2017-06-02 15:48:16 +0530

    sp condition check

--- service-loans/src/main/java/com/capitaworld/service/loans/controller/fundseeker/corporate/CorporateApplicantController.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/controller/fundseeker/corporate/CorporateApplicantController.java
@@ -44,8 +44,8 @@ public class CorporateApplicantController {
 			// request must not be null
 			Long userId = (Long) request.getAttribute(CommonUtils.USER_ID);
 
-			if (request.getAttribute(CommonUtils.USER_TYPE)
-					.equals(String.valueOf(CommonUtils.USER_TYPE_SERVICEPROVIDER))) {
+			if (CommonUtils.UserType.SERVICE_PROVIDER == 
+					((Integer) request.getAttribute(CommonUtils.USER_TYPE)).intValue()) {
 				applicantRequest.setClientId(clientId);
 			}
 
@@ -79,8 +79,8 @@ public class CorporateApplicantController {
 		// request must not be null
 		try {
 			Long id = null;
-			if (request.getAttribute(CommonUtils.USER_TYPE)
-					.equals(String.valueOf(CommonUtils.USER_TYPE_SERVICEPROVIDER))) {
+			if (CommonUtils.UserType.SERVICE_PROVIDER == 
+					((Integer) request.getAttribute(CommonUtils.USER_TYPE)).intValue()) {
 				id = clientId;
 			} else {
 				id = (Long) request.getAttribute(CommonUtils.USER_ID);
