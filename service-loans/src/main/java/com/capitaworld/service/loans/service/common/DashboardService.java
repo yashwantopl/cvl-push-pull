b45cff1e6451ef087d6235775a7a4fb1bc8bec54
kushal <kushal@Kushal.CORP.CAPITAWORLD.COM>
2017-07-01 18:31:40 +0530

    changes related to sp recentviewers

--- service-loans/src/main/java/com/capitaworld/service/loans/service/common/DashboardService.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/common/DashboardService.java
@@ -4,7 +4,7 @@ import com.capitaworld.service.loans.model.DashboardProfileResponse;
 import com.capitaworld.service.users.model.UserResponse;
 
 public interface DashboardService {
-	public DashboardProfileResponse getBasicProfileInfo(Long applicationId,Long userId) throws Exception ;
+	public DashboardProfileResponse getBasicProfileInfo(Long applicationId,Long userId,boolean isSP) throws Exception ;
 	
 	public Integer getCount(int userType) throws Exception;
 	
