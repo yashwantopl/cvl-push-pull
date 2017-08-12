124ea93bcc2f0b808cb18cded0ec2058352b111c
HarshitCW <harshit.suhagiya@capitaworld.com>
2017-08-12 10:50:50 +0530

    - add username and product and age details for admin panel

--- api-loans/src/main/java/com/capitaworld/service/loans/utils/CommonUtils.java
+++ api-loans/src/main/java/com/capitaworld/service/loans/utils/CommonUtils.java
@@ -255,4 +255,32 @@ public class CommonUtils {
 		urlsBrforeLogin.add("/loans/loan_application/getUsersRegisteredLoanDetails");
 		urlsBrforeLogin.add("/loans/loan_application/getLoanDetailsForAdminPanel");
 	}
+	
+	public static int calculateAge(Date dateOfBirth) {
+	    Calendar today = Calendar.getInstance();
+	    Calendar birthDate = Calendar.getInstance();
+	    birthDate.setTime(dateOfBirth);
+	    if (birthDate.after(today)) {
+	        throw new IllegalArgumentException("You don't exist yet");
+	    }
+	    int todayYear = today.get(Calendar.YEAR);
+	    int birthDateYear = birthDate.get(Calendar.YEAR);
+	    int todayDayOfYear = today.get(Calendar.DAY_OF_YEAR);
+	    int birthDateDayOfYear = birthDate.get(Calendar.DAY_OF_YEAR);
+	    int todayMonth = today.get(Calendar.MONTH);
+	    int birthDateMonth = birthDate.get(Calendar.MONTH);
+	    int todayDayOfMonth = today.get(Calendar.DAY_OF_MONTH);
+	    int birthDateDayOfMonth = birthDate.get(Calendar.DAY_OF_MONTH);
+	    int age = todayYear - birthDateYear;
+
+	    // If birth date is greater than todays date (after 2 days adjustment of leap year) then decrement age one year
+	    if ((birthDateDayOfYear - todayDayOfYear > 3) || (birthDateMonth > todayMonth)){
+	        age--;
+	    
+	    // If birth date and todays date are of same month and birth day of month is greater than todays day of month then decrement age
+	    } else if ((birthDateMonth == todayMonth) && (birthDateDayOfMonth > todayDayOfMonth)){
+	        age--;
+	    }
+	    return age;
+	}
 }
