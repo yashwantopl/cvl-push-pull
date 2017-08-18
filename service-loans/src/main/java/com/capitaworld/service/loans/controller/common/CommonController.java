45da4af70a4666607e48169d3150e7cdafdf9f83
jitesh <jitesh@jitesh-PC.CORP.CAPITAWORLD.COM>
2017-08-18 17:34:38 +0530

    multiple product

--- service-loans/src/main/java/com/capitaworld/service/loans/controller/common/CommonController.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/controller/common/CommonController.java
@@ -1,7 +1,6 @@
 package com.capitaworld.service.loans.controller.common;
 
 import java.util.LinkedHashMap;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,9 +18,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capitaworld.service.loans.model.LoansResponse;
-import com.capitaworld.service.loans.model.ProductMasterRequest;
 import com.capitaworld.service.loans.model.common.LongitudeLatitudeRequest;
-import com.capitaworld.service.loans.service.fundprovider.ProductMasterService;
 import com.capitaworld.service.loans.service.fundseeker.corporate.CorporateApplicantService;
 import com.capitaworld.service.loans.utils.CommonDocumentUtils;
 import com.capitaworld.service.loans.utils.CommonUtils;
@@ -37,60 +34,60 @@ import com.capitaworld.service.users.model.UsersRequest;
 public class CommonController {
 
 	private static final Logger logger = LoggerFactory.getLogger(CommonController.class);
-	
+
 	@Autowired
-	private CorporateApplicantService corporateApplicantService; 
-	
+	private CorporateApplicantService corporateApplicantService;
+
 	@Autowired
 	private UsersClient usersClient;
-	
-	@Autowired
-	private ProductMasterService productMasterService;
-	
-	
+
 	@RequestMapping(value = "/save_lat_long", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<LoansResponse> saveLatLong(@RequestBody LongitudeLatitudeRequest longLatrequest, HttpServletRequest request,
-			@RequestParam(value = "clientId", required = false) Long clientId) {
-		
+	public ResponseEntity<LoansResponse> saveLatLong(@RequestBody LongitudeLatitudeRequest longLatrequest,
+			HttpServletRequest request, @RequestParam(value = "clientId", required = false) Long clientId) {
+
 		CommonDocumentUtils.startHook(logger, "save_lat_long");
-		
-		if(CommonUtils.isObjectNullOrEmpty(longLatrequest.getLatitude()) || CommonUtils.isObjectNullOrEmpty(longLatrequest.getLongitude()) 
-				|| CommonUtils.isObjectNullOrEmpty(longLatrequest.getUserType())){
+
+		if (CommonUtils.isObjectNullOrEmpty(longLatrequest.getLatitude())
+				|| CommonUtils.isObjectNullOrEmpty(longLatrequest.getLongitude())
+				|| CommonUtils.isObjectNullOrEmpty(longLatrequest.getUserType())) {
 			logger.warn("Latitude and longitude and usertype Require to Save Lat Lon Details");
 			return new ResponseEntity<LoansResponse>(
 					new LoansResponse(CommonUtils.INVALID_REQUEST, HttpStatus.BAD_REQUEST.value()), HttpStatus.OK);
 		}
-		
+
 		Long userId = (Long) request.getAttribute(CommonUtils.USER_ID);
 		Integer userType = ((Integer) request.getAttribute(CommonUtils.USER_TYPE)).intValue();
-		
-		Long finalUserId = userId; 
+
+		Long finalUserId = userId;
 		if (CommonUtils.UserType.SERVICE_PROVIDER == userType) {
-			if (!CommonUtils.isObjectNullOrEmpty(clientId)){
-				finalUserId = clientId;	
+			if (!CommonUtils.isObjectNullOrEmpty(clientId)) {
+				finalUserId = clientId;
 			}
 			longLatrequest.setClientId(clientId);
 		}
-		
-		if(longLatrequest.getUserType() == CommonUtils.UserType.FUND_SEEKER) {
-			try{
-				if(CommonUtils.isObjectNullOrEmpty(longLatrequest.getId())){
+
+		if (longLatrequest.getUserType() == CommonUtils.UserType.FUND_SEEKER) {
+			try {
+				if (CommonUtils.isObjectNullOrEmpty(longLatrequest.getId())) {
 					logger.warn("applicationId Require to Save Lat Lon Details");
 					return new ResponseEntity<LoansResponse>(
-							new LoansResponse(CommonUtils.INVALID_REQUEST, HttpStatus.BAD_REQUEST.value()), HttpStatus.OK);
+							new LoansResponse(CommonUtils.INVALID_REQUEST, HttpStatus.BAD_REQUEST.value()),
+							HttpStatus.OK);
 				}
-				corporateApplicantService.updateLatLong(longLatrequest, userId);	
+				corporateApplicantService.updateLatLong(longLatrequest, userId);
 				logger.warn("successfully save fs long and lat value");
-				return new ResponseEntity<LoansResponse>(
-						new LoansResponse("Successfully saved", HttpStatus.OK.value()), HttpStatus.OK);
-			} catch (Exception e ){
+				return new ResponseEntity<LoansResponse>(new LoansResponse("Successfully saved", HttpStatus.OK.value()),
+						HttpStatus.OK);
+			} catch (Exception e) {
 				logger.warn("error while save fs long and lat, applicationId==>" + longLatrequest.getId());
 				e.printStackTrace();
 				return new ResponseEntity<LoansResponse>(
-						new LoansResponse(CommonUtils.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.OK);
+						new LoansResponse(CommonUtils.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR.value()),
+						HttpStatus.OK);
 			}
 		} else {
-			if(longLatrequest.getUserType() == CommonUtils.UserType.SERVICE_PROVIDER || longLatrequest.getUserType() == CommonUtils.UserType.FUND_PROVIDER) {
+			if (longLatrequest.getUserType() == CommonUtils.UserType.SERVICE_PROVIDER
+					|| longLatrequest.getUserType() == CommonUtils.UserType.FUND_PROVIDER) {
 				UserLongitudeLatitudeRequest userRequest = new UserLongitudeLatitudeRequest();
 				userRequest.setLongitude(longLatrequest.getLongitude());
 				userRequest.setLatitude(longLatrequest.getLatitude());
@@ -100,79 +97,82 @@ public class CommonController {
 					usersClient.saveLongLatValue(userRequest);
 					return new ResponseEntity<LoansResponse>(
 							new LoansResponse("Successfully save data", HttpStatus.OK.value()), HttpStatus.OK);
-				} catch (Exception e){
+				} catch (Exception e) {
 					logger.warn("Something went wrong while save long and lat value");
-					return new ResponseEntity<LoansResponse>(
-							new LoansResponse(CommonUtils.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.OK);
+					return new ResponseEntity<LoansResponse>(new LoansResponse(CommonUtils.SOMETHING_WENT_WRONG,
+							HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.OK);
 				}
-				
+
 			} else {
 				logger.warn("user type invalid when save lat long==>");
 				return new ResponseEntity<LoansResponse>(
 						new LoansResponse(CommonUtils.INVALID_REQUEST, HttpStatus.BAD_REQUEST.value()), HttpStatus.OK);
 			}
 		}
-			
-		
+
 	}
-	
+
 	@RequestMapping(value = "/get_lat_long", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<LoansResponse> getLatLong(@RequestBody LongitudeLatitudeRequest longLatrequest, HttpServletRequest request,
-			@RequestParam(value = "clientId", required = false) Long clientId) {
-		
+	public ResponseEntity<LoansResponse> getLatLong(@RequestBody LongitudeLatitudeRequest longLatrequest,
+			HttpServletRequest request, @RequestParam(value = "clientId", required = false) Long clientId) {
+
 		CommonDocumentUtils.startHook(logger, "get_lat_long");
-		
-		if(CommonUtils.isObjectNullOrEmpty(longLatrequest.getUserType())){
+
+		if (CommonUtils.isObjectNullOrEmpty(longLatrequest.getUserType())) {
 			logger.warn("Usertype Require to Get Lat Lon Details");
 			return new ResponseEntity<LoansResponse>(
 					new LoansResponse(CommonUtils.INVALID_REQUEST, HttpStatus.BAD_REQUEST.value()), HttpStatus.OK);
 		}
-		
+
 		Long userId = (Long) request.getAttribute(CommonUtils.USER_ID);
 		Integer userType = ((Integer) request.getAttribute(CommonUtils.USER_TYPE)).intValue();
-		
-		Long finalUserId = userId; 
+
+		Long finalUserId = userId;
 		if (CommonUtils.UserType.SERVICE_PROVIDER == userType) {
-			if (!CommonUtils.isObjectNullOrEmpty(clientId)){
-				finalUserId = clientId;	
+			if (!CommonUtils.isObjectNullOrEmpty(clientId)) {
+				finalUserId = clientId;
 			}
 			longLatrequest.setClientId(clientId);
 		}
-		
-		if(longLatrequest.getUserType() == CommonUtils.UserType.FUND_SEEKER) {
+
+		if (longLatrequest.getUserType() == CommonUtils.UserType.FUND_SEEKER) {
 			try {
-				if(CommonUtils.isObjectNullOrEmpty(longLatrequest.getId())){
+				if (CommonUtils.isObjectNullOrEmpty(longLatrequest.getId())) {
 					logger.warn("applicationId Require to get Lat Lon Details ==>");
 					return new ResponseEntity<LoansResponse>(
-							new LoansResponse(CommonUtils.INVALID_REQUEST, HttpStatus.BAD_REQUEST.value()), HttpStatus.OK);
+							new LoansResponse(CommonUtils.INVALID_REQUEST, HttpStatus.BAD_REQUEST.value()),
+							HttpStatus.OK);
 				}
-				LoansResponse response = new LoansResponse("Successfully get data",HttpStatus.OK.value());
-				response.setData(corporateApplicantService.getLatLonByApplicationAndUserId(longLatrequest.getId(), finalUserId));
+				LoansResponse response = new LoansResponse("Successfully get data", HttpStatus.OK.value());
+				response.setData(
+						corporateApplicantService.getLatLonByApplicationAndUserId(longLatrequest.getId(), finalUserId));
 				logger.warn("successfully get fs long and lat value");
 				return new ResponseEntity<LoansResponse>(response, HttpStatus.OK);
-			} catch (Exception e ){
+			} catch (Exception e) {
 				logger.warn("error while get fs long and lat, applicationId==>" + longLatrequest.getId());
 				e.printStackTrace();
 				return new ResponseEntity<LoansResponse>(
-						new LoansResponse(CommonUtils.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.OK);
+						new LoansResponse(CommonUtils.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR.value()),
+						HttpStatus.OK);
 			}
 		} else {
-			if(longLatrequest.getUserType() == CommonUtils.UserType.SERVICE_PROVIDER || longLatrequest.getUserType() == CommonUtils.UserType.FUND_PROVIDER) {
+			if (longLatrequest.getUserType() == CommonUtils.UserType.SERVICE_PROVIDER
+					|| longLatrequest.getUserType() == CommonUtils.UserType.FUND_PROVIDER) {
 				UserLongitudeLatitudeRequest userRequest = new UserLongitudeLatitudeRequest();
 				userRequest.setUserId(finalUserId);
 				userRequest.setUserType(longLatrequest.getUserType());
 				try {
 					UserResponse userResponse = usersClient.getLongLatValue(userRequest);
-					LoansResponse response = new LoansResponse("Successfully get data",HttpStatus.OK.value());
+					LoansResponse response = new LoansResponse("Successfully get data", HttpStatus.OK.value());
 					response.setData(userResponse.getData());
 					logger.warn("successfully get fp and sp long and lat value");
 					return new ResponseEntity<LoansResponse>(response, HttpStatus.OK);
-				} catch (Exception e){
+				} catch (Exception e) {
 					logger.warn("Something went wrong while get long and lat value");
-					return new ResponseEntity<LoansResponse>(
-							new LoansResponse(CommonUtils.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.OK);
+					return new ResponseEntity<LoansResponse>(new LoansResponse(CommonUtils.SOMETHING_WENT_WRONG,
+							HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.OK);
 				}
-				
+
 			} else {
 				logger.warn("user type invalid when get lat long==>");
 				return new ResponseEntity<LoansResponse>(
@@ -180,74 +180,80 @@ public class CommonController {
 			}
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	@RequestMapping(value = "/user_verification", method = RequestMethod.GET,  produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<UserResponse> userVerification(HttpServletRequest request,@RequestParam(value = "clientId", required = false) Long clientId){
-		
+	@RequestMapping(value = "/user_verification", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UserResponse> userVerification(HttpServletRequest request,
+			@RequestParam(value = "clientId", required = false) Long clientId) {
+
 		CommonDocumentUtils.startHook(logger, "user_verification");
 		Long userId = Long.valueOf(request.getAttribute(CommonUtils.USER_ID).toString());
 		Integer userType = (Integer) request.getAttribute(CommonUtils.USER_TYPE);
-		
+
 		if (CommonUtils.isObjectNullOrEmpty(userId) || CommonUtils.isObjectNullOrEmpty(userType)) {
 			logger.warn("Invalid Request... UserId or UserType is not found ");
-			return new ResponseEntity<UserResponse>(new UserResponse("UserId or UserType is not found",
-					HttpStatus.BAD_REQUEST.value()), HttpStatus.OK);
+			return new ResponseEntity<UserResponse>(
+					new UserResponse("UserId or UserType is not found", HttpStatus.BAD_REQUEST.value()), HttpStatus.OK);
 		}
 		JSONObject obj = new JSONObject();
 		obj.put("userType", userType);
-		
+
 		Integer spUserId = null;
 		if (CommonUtils.UserType.SERVICE_PROVIDER == userType) {
-			//SP LOGIN
-			if(!CommonUtils.isObjectNullOrEmpty(clientId)){
-				//MEANS FS, FP VIEW
+			// SP LOGIN
+			if (!CommonUtils.isObjectNullOrEmpty(clientId)) {
+				// MEANS FS, FP VIEW
 				userId = clientId;
 				try {
 					UserResponse response = usersClient.getUserTypeByUserId(new UsersRequest(userId));
-					if(response != null && response.getData() != null){
-						UserTypeRequest req = MultipleJSONObjectHelper.getObjectFromMap((LinkedHashMap<String,Object>) response.getData(), UserTypeRequest.class);
+					if (response != null && response.getData() != null) {
+						UserTypeRequest req = MultipleJSONObjectHelper.getObjectFromMap(
+								(LinkedHashMap<String, Object>) response.getData(), UserTypeRequest.class);
 						spUserId = req.getId().intValue();
 					} else {
 						logger.warn("user_verification, Invalid Request... Client Id is not valid");
-						return new ResponseEntity<UserResponse>(new UserResponse("Client Id is not valid",
-								HttpStatus.BAD_REQUEST.value()), HttpStatus.OK);
-					}	
-				} catch(Exception e) {
+						return new ResponseEntity<UserResponse>(
+								new UserResponse("Client Id is not valid", HttpStatus.BAD_REQUEST.value()),
+								HttpStatus.OK);
+					}
+				} catch (Exception e) {
 					logger.warn("user_verification, Invalid Request... Something went wrong");
 					e.printStackTrace();
-					return new ResponseEntity<UserResponse>(new UserResponse("Something went wrong",
-							HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.OK);
+					return new ResponseEntity<UserResponse>(
+							new UserResponse("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR.value()),
+							HttpStatus.OK);
 				}
 			} else {
 				spUserId = CommonUtils.UserType.SERVICE_PROVIDER;
 			}
-			userType = spUserId; 
+			userType = spUserId;
 		}
-		
-		
-		
+
 		UsersRequest usersRequest = new UsersRequest();
 		usersRequest.setId(userId);
-		if(userType == CommonUtils.UserType.FUND_SEEKER){
+		if (userType == CommonUtils.UserType.FUND_SEEKER || userType == CommonUtils.UserType.FUND_PROVIDER) {
 			try {
 				UserResponse response = usersClient.getLastAccessApplicant(usersRequest);
-				obj.put("lastAccessAppId", response.getId());	
-			} catch (Exception e){
+				obj.put("lastAccessId", response.getId());
+			} catch (Exception e) {
 				logger.warn("Error While Get Last access application id");
-				return new ResponseEntity<UserResponse>(new UserResponse("Something went wrong",
-						HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.OK);
+				return new ResponseEntity<UserResponse>(
+						new UserResponse("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR.value()),
+						HttpStatus.OK);
 			}
-		} else if(userType == CommonUtils.UserType.FUND_PROVIDER){
-			List<ProductMasterRequest> productMasterlist = productMasterService.getList(userId);
-			obj.put("productId", !CommonUtils.isListNullOrEmpty(productMasterlist) ? productMasterlist.get(0).getId() : null);
-		} else if(userType == CommonUtils.UserType.SERVICE_PROVIDER){
-			obj.put("productId",null);
-			obj.put("lastAccessAppId",null);
+		} /*
+			 * else if(userType == CommonUtils.UserType.FUND_PROVIDER){
+			 * List<ProductMasterRequest> productMasterlist =
+			 * productMasterService.getList(userId); obj.put("productId",
+			 * !CommonUtils.isListNullOrEmpty(productMasterlist) ?
+			 * productMasterlist.get(0).getId() : null); }
+			 */ else if (userType == CommonUtils.UserType.SERVICE_PROVIDER) {
+			obj.put("productId", null);
+			obj.put("lastAccessAppId", null);
 		}
 		CommonDocumentUtils.endHook(logger, "user_verification");
-		return new ResponseEntity<UserResponse>(new UserResponse(obj,"Successfully get data",
-				HttpStatus.OK.value()), HttpStatus.OK);
+		return new ResponseEntity<UserResponse>(new UserResponse(obj, "Successfully get data", HttpStatus.OK.value()),
+				HttpStatus.OK);
 	}
-	
+
 }
