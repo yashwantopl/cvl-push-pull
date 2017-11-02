cc8ea4081cfc1bafcde70678edccbb913391d1b1
garima <garima@win-pc.CORP.CAPITAWORLD.COM>
2017-11-02 15:51:22 +0530

    unsecured co-applicant detail added

---
+++ api-loans/src/main/java/com/capitaworld/service/loans/model/corporate/UnsecuredCoApplicantDetail.java
@@ -0,0 +1,227 @@
+package com.capitaworld.service.loans.model.corporate;
+
+public class UnsecuredCoApplicantDetail {
+
+	private Long id;
+
+	private Long administrativeCityId;
+
+	private Integer administrativeCountryId;
+
+	private String administrativeLandMark;
+
+	private Long administrativePincode;
+
+	private String administrativePremiseNumber;
+
+	private Integer administrativeStateId;
+
+	private String administrativeStreetName;
+
+	private Integer constitutionId;
+
+	private String organisationName;
+
+	private Long registeredCityId;
+
+	private Integer registeredCountryId;
+
+	private String registeredLandMark;
+
+	private Long registeredPincode;
+
+	private String registeredPremiseNumber;
+
+	private Integer registeredStateId;
+
+	private String registeredStreetName;
+
+	private Boolean sameAs;
+
+	private String aadharNumber;
+
+	private String panNo;
+
+	private String landlineNo;
+
+	private Integer relationshipWithApplicant;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getAdministrativeCityId() {
+		return administrativeCityId;
+	}
+
+	public void setAdministrativeCityId(Long administrativeCityId) {
+		this.administrativeCityId = administrativeCityId;
+	}
+
+	public Integer getAdministrativeCountryId() {
+		return administrativeCountryId;
+	}
+
+	public void setAdministrativeCountryId(Integer administrativeCountryId) {
+		this.administrativeCountryId = administrativeCountryId;
+	}
+
+	public String getAdministrativeLandMark() {
+		return administrativeLandMark;
+	}
+
+	public void setAdministrativeLandMark(String administrativeLandMark) {
+		this.administrativeLandMark = administrativeLandMark;
+	}
+
+	public Long getAdministrativePincode() {
+		return administrativePincode;
+	}
+
+	public void setAdministrativePincode(Long administrativePincode) {
+		this.administrativePincode = administrativePincode;
+	}
+
+	public String getAdministrativePremiseNumber() {
+		return administrativePremiseNumber;
+	}
+
+	public void setAdministrativePremiseNumber(String administrativePremiseNumber) {
+		this.administrativePremiseNumber = administrativePremiseNumber;
+	}
+
+	public Integer getAdministrativeStateId() {
+		return administrativeStateId;
+	}
+
+	public void setAdministrativeStateId(Integer administrativeStateId) {
+		this.administrativeStateId = administrativeStateId;
+	}
+
+	public String getAdministrativeStreetName() {
+		return administrativeStreetName;
+	}
+
+	public void setAdministrativeStreetName(String administrativeStreetName) {
+		this.administrativeStreetName = administrativeStreetName;
+	}
+
+	public Integer getConstitutionId() {
+		return constitutionId;
+	}
+
+	public void setConstitutionId(Integer constitutionId) {
+		this.constitutionId = constitutionId;
+	}
+
+	public String getOrganisationName() {
+		return organisationName;
+	}
+
+	public void setOrganisationName(String organisationName) {
+		this.organisationName = organisationName;
+	}
+
+	public Long getRegisteredCityId() {
+		return registeredCityId;
+	}
+
+	public void setRegisteredCityId(Long registeredCityId) {
+		this.registeredCityId = registeredCityId;
+	}
+
+	public Integer getRegisteredCountryId() {
+		return registeredCountryId;
+	}
+
+	public void setRegisteredCountryId(Integer registeredCountryId) {
+		this.registeredCountryId = registeredCountryId;
+	}
+
+	public String getRegisteredLandMark() {
+		return registeredLandMark;
+	}
+
+	public void setRegisteredLandMark(String registeredLandMark) {
+		this.registeredLandMark = registeredLandMark;
+	}
+
+	public Long getRegisteredPincode() {
+		return registeredPincode;
+	}
+
+	public void setRegisteredPincode(Long registeredPincode) {
+		this.registeredPincode = registeredPincode;
+	}
+
+	public String getRegisteredPremiseNumber() {
+		return registeredPremiseNumber;
+	}
+
+	public void setRegisteredPremiseNumber(String registeredPremiseNumber) {
+		this.registeredPremiseNumber = registeredPremiseNumber;
+	}
+
+	public Integer getRegisteredStateId() {
+		return registeredStateId;
+	}
+
+	public void setRegisteredStateId(Integer registeredStateId) {
+		this.registeredStateId = registeredStateId;
+	}
+
+	public String getRegisteredStreetName() {
+		return registeredStreetName;
+	}
+
+	public void setRegisteredStreetName(String registeredStreetName) {
+		this.registeredStreetName = registeredStreetName;
+	}
+
+	public Boolean getSameAs() {
+		return sameAs;
+	}
+
+	public void setSameAs(Boolean sameAs) {
+		this.sameAs = sameAs;
+	}
+
+	public String getAadharNumber() {
+		return aadharNumber;
+	}
+
+	public void setAadharNumber(String aadharNumber) {
+		this.aadharNumber = aadharNumber;
+	}
+
+	public String getPanNo() {
+		return panNo;
+	}
+
+	public void setPanNo(String panNo) {
+		this.panNo = panNo;
+	}
+
+	public String getLandlineNo() {
+		return landlineNo;
+	}
+
+	public void setLandlineNo(String landlineNo) {
+		this.landlineNo = landlineNo;
+	}
+
+	public Integer getRelationshipWithApplicant() {
+		return relationshipWithApplicant;
+	}
+
+	public void setRelationshipWithApplicant(Integer relationshipWithApplicant) {
+		this.relationshipWithApplicant = relationshipWithApplicant;
+	}
+
+	
+	
+}
