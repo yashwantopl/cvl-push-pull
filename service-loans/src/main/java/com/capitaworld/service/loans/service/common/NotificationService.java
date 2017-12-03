1de7007ca1047205563249e3ca2cf1c9063d2202
SanketCW <sanket.devare@capitaworld.com>
2017-12-03 16:07:54 +0530

    #CW-1197

--- service-loans/src/main/java/com/capitaworld/service/loans/service/common/NotificationService.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/common/NotificationService.java
@@ -1,9 +1,15 @@
 package com.capitaworld.service.loans.service.common;
 
+import java.util.Map;
+
 import com.capitaworld.service.loans.utils.CommonNotificationUtils.NotificationTemplate;
 
 public interface NotificationService {
 
 	public void sendViewNotification(String toUserId, Long fromUserId, Long fromUserTypeId, Long notificationId,Long applicationId,Long fpProductId, NotificationTemplate notificationTemplate,Long loginUserType);
+
+	public void createEmailNotificationForUBI(String[] toIds, Long fromId, Long fromUserTypeId, Long templateId,
+			Long fromUserId, Map<String, Object> parameters, Long applicationId, Long fpProductId,
+			NotificationTemplate notificationTemplate, String fpName);
 	
 }
