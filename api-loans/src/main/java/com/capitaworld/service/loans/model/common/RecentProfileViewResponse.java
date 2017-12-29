d30d52e0008cffbbcd800259f4af39823d45a9ac
bihag <bihag.gandhi@capitaworld.com>
2017-12-29 11:40:22 +0530

    cw-1232 modified recent-profile-view page

--- api-loans/src/main/java/com/capitaworld/service/loans/model/common/RecentProfileViewResponse.java
+++ api-loans/src/main/java/com/capitaworld/service/loans/model/common/RecentProfileViewResponse.java
@@ -19,11 +19,67 @@ public class RecentProfileViewResponse implements Serializable {
 	private String proPicFileName;
 	private Long userId;
 	private Long applicationId;
-	private Long productId;
+	private Long productId;	
+	private String fpProductName;
+	private String whoAreYou;
+	private String fpType;
+	private String imagePath;
+	private Long proposalMappingId;
+	private Double elAmount;
+	private Double elTenure;
+	private Double elRoi;
 	
 	
 	
 	
+	public String getFpProductName() {
+		return fpProductName;
+	}
+	public void setFpProductName(String fpProductName) {
+		this.fpProductName = fpProductName;
+	}
+	public String getWhoAreYou() {
+		return whoAreYou;
+	}
+	public void setWhoAreYou(String whoAreYou) {
+		this.whoAreYou = whoAreYou;
+	}
+	public String getFpType() {
+		return fpType;
+	}
+	public void setFpType(String fpType) {
+		this.fpType = fpType;
+	}
+	public String getImagePath() {
+		return imagePath;
+	}
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+	public Long getProposalMappingId() {
+		return proposalMappingId;
+	}
+	public void setProposalMappingId(Long proposalMappingId) {
+		this.proposalMappingId = proposalMappingId;
+	}
+	public Double getElAmount() {
+		return elAmount;
+	}
+	public void setElAmount(Double elAmount) {
+		this.elAmount = elAmount;
+	}
+	public Double getElTenure() {
+		return elTenure;
+	}
+	public void setElTenure(Double elTenure) {
+		this.elTenure = elTenure;
+	}
+	public Double getElRoi() {
+		return elRoi;
+	}
+	public void setElRoi(Double elRoi) {
+		this.elRoi = elRoi;
+	}
 	public Long getUserId() {
 		return userId;
 	}
