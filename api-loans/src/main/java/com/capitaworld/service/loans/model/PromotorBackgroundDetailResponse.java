339cb2271dfb653877d13576f02cf8f329abe5aa
AkshayCW <akshay.patel@capitaworld.com>
2018-02-05 12:32:32 +0530

    Change

--- api-loans/src/main/java/com/capitaworld/service/loans/model/PromotorBackgroundDetailResponse.java
+++ api-loans/src/main/java/com/capitaworld/service/loans/model/PromotorBackgroundDetailResponse.java
@@ -8,81 +8,81 @@ import java.io.Serializable;
  * Created by dhaval on 21-May-17.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class PromotorBackgroundDetailResponse implements Serializable {
+public class PromotorBackgroundDetailResponse implements Serializable{
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private String achivements;
+    private String achievements;
 
-	private String address;
+    private String address;
 
-	private Double age;
+    private Double age;
 
-	private String panNo;
+    private String panNo;
 
-	private String promotorsName;
+    private String promotorsName;
 
-	private String qualification;
+    private String qualification;
 
-	private Double totalExperience;
+    private Double totalExperience;
+    
+    private Double networth;
 
-	private Double networth;
+    public String getAchievements() {
+        return achievements;
+    }
 
-	public String getAchivements() {
-		return achivements;
-	}
+    public void setAchievements(String achievements) {
+        this.achievements = achievements;
+    }
 
-	public void setAchivements(String achivements) {
-		this.achivements = achivements;
-	}
+    public String getAddress() {
+        return address;
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    public Double getAge() {
+        return age;
+    }
 
-	public Double getAge() {
-		return age;
-	}
+    public void setAge(Double age) {
+        this.age = age;
+    }
 
-	public void setAge(Double age) {
-		this.age = age;
-	}
+    public String getPanNo() {
+        return panNo;
+    }
 
-	public String getPanNo() {
-		return panNo;
-	}
+    public void setPanNo(String panNo) {
+        this.panNo = panNo;
+    }
 
-	public void setPanNo(String panNo) {
-		this.panNo = panNo;
-	}
+    public String getPromotorsName() {
+        return promotorsName;
+    }
 
-	public String getPromotorsName() {
-		return promotorsName;
-	}
+    public void setPromotorsName(String promotorsName) {
+        this.promotorsName = promotorsName;
+    }
 
-	public void setPromotorsName(String promotorsName) {
-		this.promotorsName = promotorsName;
-	}
+    public String getQualification() {
+        return qualification;
+    }
 
-	public String getQualification() {
-		return qualification;
-	}
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
 
-	public void setQualification(String qualification) {
-		this.qualification = qualification;
-	}
+    public Double getTotalExperience() {
+        return totalExperience;
+    }
 
-	public Double getTotalExperience() {
-		return totalExperience;
-	}
-
-	public void setTotalExperience(Double totalExperience) {
-		this.totalExperience = totalExperience;
-	}
+    public void setTotalExperience(Double totalExperience) {
+        this.totalExperience = totalExperience;
+    }
 
 	public Double getNetworth() {
 		return networth;
@@ -91,5 +91,7 @@ public class PromotorBackgroundDetailResponse implements Serializable {
 	public void setNetworth(Double networth) {
 		this.networth = networth;
 	}
-
+	
+    
+    
 }
