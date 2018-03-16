80a41d42fc027b87c169b0173f488075b065ff7a
RavinaCW <ravina.panchal@capitaworld.com>
2018-03-16 15:35:34 +0530

    SB oneform and teaser changes

--- sql/mode_of_receipt.sql
+++ sql/mode_of_receipt.sql
@@ -1,1 +1,3 @@
 alter table	loan_application.fs_retail_applicant_details add column mode_of_receipt int default null;
+alter table	loan_application.fs_retail_co_applicant_details add column mode_of_receipt int default null;
+alter table	loan_application.fs_retail_guarantor_details add column mode_of_receipt int default null;
