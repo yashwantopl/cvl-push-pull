232ee10131b33d16539959ebb45a4136ca81a53e
HirenCW <hiren@capitaworld.com>
2018-04-06 16:42:44 +0530

    fs request data saved updated

---
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/FundSeekerInputRequestService.java
@@ -0,0 +1,8 @@
+package com.capitaworld.service.loans.service.fundseeker.corporate;
+
+import com.capitaworld.service.loans.model.corporate.FundSeekerInputRequest;
+
+public interface FundSeekerInputRequestService {
+
+    public Boolean saveOrUpdate(FundSeekerInputRequest fundSeekerInputRequest);
+}
