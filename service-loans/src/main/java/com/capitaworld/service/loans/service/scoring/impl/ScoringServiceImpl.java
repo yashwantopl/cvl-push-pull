856a2a52d77caa8dd35720d279d75b8f7d0b076c
HirenCW <hiren@capitaworld.com>
2018-04-12 19:47:15 +0530

    calculation changed

--- service-loans/src/main/java/com/capitaworld/service/loans/service/scoring/impl/ScoringServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/scoring/impl/ScoringServiceImpl.java
@@ -107,10 +107,10 @@ public class ScoringServiceImpl implements ScoringService{
 
         GstResponse gstResponse=null;
         GstCalculation gstCalculation=new GstCalculation();
-     /*   gstCalculation.setConcentration(20d);
+        gstCalculation.setConcentration(20d);
         gstCalculation.setNoOfCustomer(223d);
-        gstCalculation.setProjectedSales(1000000d);*/
-        try
+        gstCalculation.setProjectedSales(1000000d);
+       /* try
         {
             GSTR1Request gstr1Request=new GSTR1Request();
             gstr1Request.setGstin(gstNumber);
@@ -127,7 +127,7 @@ public class ScoringServiceImpl implements ScoringService{
         {
             logger.error("error while getting GST parameter");
             e.printStackTrace();
-        }
+        }*/
         // end Get GST Parameter
 
         int currentYear = Calendar.getInstance().get(Calendar.YEAR);
