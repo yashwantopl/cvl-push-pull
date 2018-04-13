2a5becc12ee2c83310640839a4e65d7bfd8f0658
AkshayCW <akshay.patel@capitaworld.com>
2018-04-13 20:27:22 +0530

    commit change

--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/impl/FundSeekerInputRequestServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/impl/FundSeekerInputRequestServiceImpl.java
@@ -130,12 +130,11 @@ public class FundSeekerInputRequestServiceImpl implements FundSeekerInputRequest
             // save and commit transaction
 
             EntityManagerFactory emf = jpaTransactionManager.getEntityManagerFactory();
-            emf.close();
-//            EntityManager entityManager = EntityManagerFactoryUtils.getTransactionalEntityManager(emf);
+            EntityManager entityManager = EntityManagerFactoryUtils.getTransactionalEntityManager(emf);
 //            EntityTransaction entityTransaction= entityManager.getTransaction();
 //            entityTransaction.commit();
-//            entityManager.flush();
-//            entityManager.clear();
+            entityManager.flush();
+            entityManager.clear();
 
 
 
