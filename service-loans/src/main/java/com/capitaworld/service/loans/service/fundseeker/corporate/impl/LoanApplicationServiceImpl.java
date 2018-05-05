0fd7d0438d3193fd425084604499bb1d40c557fc
KushalCW <kushal@capitaworld.com>
2018-05-06 00:05:37 +0530

    update

--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/impl/LoanApplicationServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/impl/LoanApplicationServiceImpl.java
@@ -4823,7 +4823,7 @@ public class LoanApplicationServiceImpl implements LoanApplicationService {
 			}
 
 			//To save irr details
-			RatingResponse ratingResponse = irrService.calculateIrrRating(applicationId,applicationMaster.getUserId()).getBody();
+			RatingResponse rtResponse = irrService.calculateIrrRating(applicationId,applicationMaster.getUserId()).getBody();
 			RatingResponse ratingResponse = (RatingResponse)rtResponse.getData();
 			logger.info("ratingResponse response ----->" + ratingResponse.toString());
 			com.capitaworld.sidbi.integration.model.irr.IrrRequest irrRequest = new com.capitaworld.sidbi.integration.model.irr.IrrRequest();
