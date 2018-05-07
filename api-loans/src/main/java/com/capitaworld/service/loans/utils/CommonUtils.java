43cf04fc728f2b775d0eada82073e121933cb638
JiteshCW <jitesh@capitaworld.com>
2018-05-07 11:27:10 +0530

    added loan type

--- api-loans/src/main/java/com/capitaworld/service/loans/utils/CommonUtils.java
+++ api-loans/src/main/java/com/capitaworld/service/loans/utils/CommonUtils.java
@@ -136,7 +136,7 @@ public class CommonUtils {
 
 	public enum LoanType {
 		WORKING_CAPITAL(1,"Working Capital","WC"), TERM_LOAN(2,"Term Loan","TL"), HOME_LOAN(3,"Home Loan","HL"), CAR_LOAN(12,"Car Loan","CL"), PERSONAL_LOAN(7,"Personal Loan","PL"), LAP_LOAN(13,"Loan Against Property","LAP"), LAS_LOAN(
-				14,"Loan Against Shares","LAS"), UNSECURED_LOAN(15,"UnSecured Loan","USL");
+				14,"Loan Against Shares","LAS"), UNSECURED_LOAN(15,"UnSecured Loan","USL"), WC_TL_LOAN(15,"Working Capital Term Loan","wctl");
 		private int value;
 		private String name;
 		private String code;
