e92dc36e5efbbf1c6ea85a82c4ac3a4ebcaa8c04
AnkitCW <ankit.gupta@capitaworld.com>
2018-06-14 01:08:39 +0530

    change client call

--- service-loans/src/main/java/com/capitaworld/service/loans/service/sanction/LoanDisbursementService.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/sanction/LoanDisbursementService.java
@@ -8,7 +8,9 @@ import com.capitaworld.service.loans.model.LoansResponse;
 public interface LoanDisbursementService {
 	public Boolean saveLoanDisbursementDetail(LoanDisbursementRequest loanDisbursementRequest)throws IOException ;
 	
-	public String requestValidation(LoanDisbursementRequest loanDisbursementRequest)throws IOException ;
+	public String requestValidation(LoanDisbursementRequest loanDisbursementRequest, Long orgId)throws IOException ;
 	
 	public void saveBankReqRes(LoanDisbursementRequest loanDisbursementRequest, LoansResponse loansResponse, String msg, Long orgId) throws IOException  ;
+	
+	public Long getOrgIdByCredential(String userName, String pwd) ;
 }
