7b99b8a9cd0307bd56024e5c5db3d265915f9244
AnkitCW <ankit.gupta@capitaworld.com>
2018-06-13 18:46:52 +0530

    Store req and response bank to CWDB change in sanction and disbursement response

--- service-loans/src/main/java/com/capitaworld/service/loans/repository/sanction/LoanDisbursementRepository.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/repository/sanction/LoanDisbursementRepository.java
@@ -1,6 +1,9 @@
 package com.capitaworld.service.loans.repository.sanction;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.capitaworld.service.loans.domain.sanction.LoanDisbursementDomain;
 /**
  * @author Ankit
@@ -8,4 +11,6 @@ import com.capitaworld.service.loans.domain.sanction.LoanDisbursementDomain;
  */
 public interface LoanDisbursementRepository extends JpaRepository<LoanDisbursementDomain, Long> {
 
+	@Query("SELECT sum(ldd.disbursedAmount) FROM  LoanDisbursementDomain ldd WHERE ldd.applicationId =:applcationId and ldd.isActive =true")
+	public Double getTotalDisbursedAmoount(@Param("applicationId") Long applicationId );
 }
