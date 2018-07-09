681802ccfeec979a121ed0eee031ce8ea36e1b96
AnkitCW <ankit.gupta@capitaworld.com>
2018-07-09 12:31:20 +0530

    changes

--- service-loans/src/main/java/com/capitaworld/service/loans/repository/token/TokenRepository.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/repository/token/TokenRepository.java
@@ -1,5 +1,7 @@
 package com.capitaworld.service.loans.repository.token;
 
+import java.util.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -7,10 +9,10 @@ import org.springframework.data.repository.query.Param;
 
 import com.capitaworld.service.loans.domain.token.TokenDetail;
 
-public interface TokenRepository extends JpaRepository<TokenDetail,Long>{ 
+public interface TokenRepository extends JpaRepository<TokenDetail,Long>{
 
-	@Query("select td FROM TokenDetail td WHERE td.token =:token AND td.isExpired = false AND td.createdDate = date_sub( NOW() , INTERVAL :time MINUTE )   AND td.isActive = true")
-	public TokenDetail getTokenByApplicationId(@Param("token") String tokenString, @Param("time") Long  time);
+	@Query("select td FROM TokenDetail td WHERE td.token =:token AND td.isExpired = false AND  (time(now())- time(createdDate)) <= time( :time ) AND td.isActive = true")
+	public TokenDetail getTokenByApplicationId(@Param("token") String tokenString, @Param("time") Date time);
 
 	@Modifying
 	@Query("UPDATE TokenDetail td SET td.isExpired = true , td.isActive = false , td.modifiedDate = now() WHERE td.applicationId =:applicationId AND td.isActive = true ")
