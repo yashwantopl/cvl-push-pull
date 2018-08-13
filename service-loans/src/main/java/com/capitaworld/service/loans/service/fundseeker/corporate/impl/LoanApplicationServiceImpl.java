7f052ee70b52d4476deca09a2b171fec1300eded
AkshayCW <akshay.patel@capitaworld.com>
2018-08-13 22:14:58 +0530

    Mange code

--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/impl/LoanApplicationServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/impl/LoanApplicationServiceImpl.java
@@ -7311,7 +7311,7 @@ public CommercialRequest createCommercialRequest(Long applicationId,String pan) 
 		generateTokenRequest.setApplicationId(applicationId);
 		generateTokenRequest.setPassword(request.getPassword());
 		String bankToken = null;
-		if(organizationId == 17l) {
+		if(organizationId == 16l) {
 			String reqTok = "bobc:bob12345";
 			String requestDataEnc = Base64.getEncoder().encodeToString(reqTok.getBytes());
 			generateTokenRequest.setBankToken(requestDataEnc);
