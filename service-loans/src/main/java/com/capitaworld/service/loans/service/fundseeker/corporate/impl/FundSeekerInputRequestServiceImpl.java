439abccfa789603dbfab2a3b8d7fa7e2a92f53d8
RavinaCW <ravina.panchal@capitaworld.com>
2018-08-14 19:36:35 +0530

    maindir condition check

--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/impl/FundSeekerInputRequestServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/impl/FundSeekerInputRequestServiceImpl.java
@@ -410,7 +410,7 @@ public class FundSeekerInputRequestServiceImpl implements FundSeekerInputRequest
 
 				directorBackgroundDetailRequest = new DirectorBackgroundDetailRequest();
 				BeanUtils.copyProperties(directorBackgroundDetail, directorBackgroundDetailRequest);
-				if(directorBackgroundDetail.getIsMainDirector() && !CommonUtils.isObjectNullOrEmpty(directorBackgroundDetail.getDirectorPersonalDetail())){
+				if(!CommonUtils.isObjectNullOrEmpty(directorBackgroundDetail.getIsMainDirector()) && (directorBackgroundDetail.getIsMainDirector()) && !CommonUtils.isObjectNullOrEmpty(directorBackgroundDetail.getDirectorPersonalDetail())){
 					DirectorPersonalDetailRequest directorPersonalDetailRequest = new DirectorPersonalDetailRequest();
 					BeanUtils.copyProperties(directorBackgroundDetail.getDirectorPersonalDetail(), directorPersonalDetailRequest);
 					directorBackgroundDetailRequest.setDirectorPersonalDetailRequest(directorPersonalDetailRequest);
