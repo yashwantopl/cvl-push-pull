c3de71e24cc645982b90eb8c03e8b2203a0b2900
DhavalCW <dhaval@capitaworld.com>
2018-08-15 11:27:05 +0530

    pincode functionality completed

--- service-loans/src/main/java/com/capitaworld/service/loans/repository/PincodeDataRepository.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/repository/PincodeDataRepository.java
@@ -1,7 +1,14 @@
 package com.capitaworld.service.loans.repository;
 
 import com.capitaworld.service.loans.domain.PincodeData;
+import com.capitaworld.service.loans.model.PincodeDataResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PincodeDataRepository extends JpaRepository<PincodeData,Long> {
+
+    List<PincodeData> findAllByPincode(String pincode);
 }
