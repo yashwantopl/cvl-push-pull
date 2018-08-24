86e5f62ec165afc0106493ad2f0adeb0fe474407
SanketCW <sanket.devare@capitaworld.com>
2018-08-24 20:41:53 +0530

    Cron Changed to (59 59 23 * * *) from (0 0 24 * * *)

--- service-loans/src/main/java/com/capitaworld/service/loans/scheduler/APIScheduled.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/scheduler/APIScheduled.java
@@ -16,7 +16,7 @@ public class APIScheduled {
 	@Autowired
 	private LoanSanctionService loanSanctionService;
 	
-	@Scheduled(cron = " 0 0 24 * * *")
+	@Scheduled(cron = "59 59 23 * * *")
 	public void saveSanctionAndDisbursementDetailFromBank() {
 		logger.info("============ start calling saveSanctionAndDisbursementDetailFromBank(){} call at scheduled time==============  ");
 		try {
