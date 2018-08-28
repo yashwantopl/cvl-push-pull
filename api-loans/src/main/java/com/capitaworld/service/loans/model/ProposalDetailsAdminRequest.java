c0836a883ef83545c8b372caf67ee954d91f11f1
DhavalCW <dhaval@capitaworld.com>
2018-08-28 11:36:09 +0530

    admin proposal code added

---
+++ api-loans/src/main/java/com/capitaworld/service/loans/model/ProposalDetailsAdminRequest.java
@@ -0,0 +1,199 @@
+package com.capitaworld.service.loans.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProposalDetailsAdminRequest implements Serializable {
+
+    private Long applicationId;
+    private Long userId;
+    private String email;
+    private String organizationName;
+    private Integer stageId;
+    private Long status;
+    private String message;
+    private Long fpProductId;
+    private String bankName;
+    private String productName;
+    private String modifiedDate;
+    private String signUpDate;
+    private String loanAmount;
+    private String tenure;
+    private String rate;
+    private Long branchId;
+    private Long userOrgId;
+    private String city;
+    private String state;
+    private String branchName;
+    private String bankImageUrl;
+
+    public Long getApplicationId() {
+        return applicationId;
+    }
+
+    public void setApplicationId(Long applicationId) {
+        this.applicationId = applicationId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+
+    public Integer getStageId() {
+        return stageId;
+    }
+
+    public void setStageId(Integer stageId) {
+        this.stageId = stageId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Long getFpProductId() {
+        return fpProductId;
+    }
+
+    public void setFpProductId(Long fpProductId) {
+        this.fpProductId = fpProductId;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(String modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public Long getStatus() {
+        return status;
+    }
+
+    public void setStatus(Long status) {
+        this.status = status;
+    }
+
+    public String getSignUpDate() {
+        return signUpDate;
+    }
+
+    public void setSignUpDate(String signUpDate) {
+        this.signUpDate = signUpDate;
+    }
+
+    public String getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(String loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public String getTenure() {
+        return tenure;
+    }
+
+    public void setTenure(String tenure) {
+        this.tenure = tenure;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    public Long getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(Long branchId) {
+        this.branchId = branchId;
+    }
+
+    public Long getUserOrgId() {
+        return userOrgId;
+    }
+
+    public void setUserOrgId(Long userOrgId) {
+        this.userOrgId = userOrgId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public String getBankImageUrl() {
+        return bankImageUrl;
+    }
+
+    public void setBankImageUrl(String bankImageUrl) {
+        this.bankImageUrl = bankImageUrl;
+    }
+}
