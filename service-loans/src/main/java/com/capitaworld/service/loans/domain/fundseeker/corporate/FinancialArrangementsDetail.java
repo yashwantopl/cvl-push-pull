5ddd88bce1e33254d60ae41444a1861d70671a27
KushalCW <kushal@capitaworld.com>
2018-09-04 17:05:15 +0530

    key added otherbankname

--- service-loans/src/main/java/com/capitaworld/service/loans/domain/fundseeker/corporate/FinancialArrangementsDetail.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/domain/fundseeker/corporate/FinancialArrangementsDetail.java
@@ -90,6 +90,9 @@ public class FinancialArrangementsDetail implements Serializable {
 	 * */
 	@Column(name = "lc_bg_status")
 	private Integer lcBgStatus;
+	
+	@Column(name = "others_bank_name")
+	private String othersBankName;
 
 	public FinancialArrangementsDetail() {
 	}
@@ -252,5 +255,13 @@ public class FinancialArrangementsDetail implements Serializable {
 
 	public void setLcBgStatus(Integer lcBgStatus) {
 		this.lcBgStatus = lcBgStatus;
+	}
+	
+	public String getOthersBankName() {
+		return othersBankName;
+	}
+
+	public void setOthersBankName(String othersBankName) {
+		this.othersBankName = othersBankName;
 	}
 }
