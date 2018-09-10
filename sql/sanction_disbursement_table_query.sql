410395db9fc53a52c0d9b733d596c174bb5e9e5b
AnkitCW <ankit.gupta@capitaworld.com>
2018-09-10 17:41:25 +0530

    add new column in bank_primary_key in bank to cw audti trail table

--- sql/sanction_disbursement_table_query.sql
+++ sql/sanction_disbursement_table_query.sql
@@ -56,3 +56,10 @@ ADD COLUMN `transaction_no` VARCHAR(45) NULL AFTER `account_no`;
 
 ALTER TABLE `loan_application`.`disbursement_detail` 
 CHANGE COLUMN `application_id` `application_id` BIGINT(11) UNSIGNED NULL DEFAULT NULL ;
+
+
+// add new column in bank_cw_audit_trail -- bank_primary_key 
+ALTER TABLE `loan_application`.`bank_cw_audit_trail` ADD COLUMN `bank_primary_key` BIGINT(19) NULL AFTER `is_active`; 
+
+
+
