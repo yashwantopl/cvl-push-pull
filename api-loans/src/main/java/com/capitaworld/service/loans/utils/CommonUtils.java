120df627f139ef19fe162972d03e2f939bf711e9
bihag.gandhi <bihag.gandhi@CWDT012.CORP.CAPITAWORLD.COM>
2018-10-04 17:46:57 +0530

    Changed cam for assessment of limit

--- api-loans/src/main/java/com/capitaworld/service/loans/utils/CommonUtils.java
+++ api-loans/src/main/java/com/capitaworld/service/loans/utils/CommonUtils.java
@@ -1273,7 +1273,7 @@ public enum APIFlags {
 		 }
 		return obj;
 	}
-	public static Object printFields(Object obj) throws Exception {
+	public static Object printFields(Object obj, Map<String, Object>data) throws Exception {
 		if(obj != null) {
 			if(obj.getClass().isArray()) {
 		}
@@ -1283,12 +1283,12 @@ public enum APIFlags {
 		if(obj instanceof List) {
 			List<?> lst = (List)obj;
 			for(Object o : lst) {
-				o = printFields(o);
+				o = printFields(o,data);
 			}
 		}else if(obj instanceof Map) {
 			Map<Object, Object> map = (Map)obj;
 			for(Map.Entry<Object, Object> setEntry : map.entrySet()) {
-				setEntry.setValue(printFields(setEntry.getValue()));
+				setEntry.setValue(printFields(setEntry.getValue(),data));
 			}
 		}else if(obj instanceof String) {
 			obj = StringEscapeUtils.escapeXml((String)obj);
@@ -1305,7 +1305,7 @@ public enum APIFlags {
 					}else {
 						field.setAccessible(true);
 						Object value = field.get(obj);
-						field.set(obj, printFields(value));	
+						field.set(obj, printFields(value,data));	
 					}
 				}
 			}
