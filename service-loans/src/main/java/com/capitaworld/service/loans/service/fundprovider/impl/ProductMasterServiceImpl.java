faba1e7c95094421d49a97bd69ecadf3ec05be54
jitesh.savaliya <jitesh.savaliya@CWDT011.CORP.CAPITAWORLD.COM>
2018-10-20 18:24:09 +0530

    added finid in product

--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundprovider/impl/ProductMasterServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundprovider/impl/ProductMasterServiceImpl.java
@@ -87,6 +87,7 @@ import com.capitaworld.service.loans.service.fundprovider.WorkingCapitalParamete
 import com.capitaworld.service.loans.utils.CommonDocumentUtils;
 import com.capitaworld.service.loans.utils.CommonUtils;
 import com.capitaworld.service.loans.utils.MultipleJSONObjectHelper;
+import com.capitaworld.service.loans.utils.CommonUtils.UserMainType;
 import com.capitaworld.service.matchengine.ProposalDetailsClient;
 import com.capitaworld.service.matchengine.exception.MatchException;
 import com.capitaworld.service.matchengine.model.ProposalMappingRequest;
@@ -365,6 +366,7 @@ public class ProductMasterServiceImpl implements ProductMasterService {
 				// set business type id
 				productMaster.setBusinessTypeId(addProductRequest.getBusinessTypeId());
 				productMaster.setWcRenewalStatus(addProductRequest.getWcRenewalStatus());
+				productMaster.setFinId(addProductRequest.getFinId());
 				productMaster.setIsCopied(false);
 				productMaster.setIsActive(true);
 				productMaster.setUserOrgId(userOrgId);
@@ -1381,10 +1383,22 @@ public class ProductMasterServiceImpl implements ProductMasterService {
 		List<ProductMaster> results = null;
 		List<ProductMasterRequest> productMasterRequests = new ArrayList<>();
 		
+			if(CommonUtils.getUserMainType(productId)==UserMainType.RETAIL)
+			{
+				if (!CommonUtils.isObjectNullOrEmpty(userOrgId)) {
+					results = productMasterRepository.getUserRetailProductListByOrgId(userOrgId);
+				} else {
+					results = productMasterRepository.getUserRetailProductList(userId);
+				}
+			}
+		
+			else
+			{
 			if (!CommonUtils.isObjectNullOrEmpty(userOrgId)) {
 				results = productMasterRepository.getUserCorporateProductListByOrgId(userOrgId);
 			} else {
 				results = productMasterRepository.getUserCorporateProductList(userId);
+			}
 			}
 		
 		
