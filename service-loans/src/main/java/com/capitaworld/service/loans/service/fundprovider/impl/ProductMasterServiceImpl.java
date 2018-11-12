901cdcdc7e9634bffaf4c7a546579152084bffa5
jitesh.savaliya <jitesh.savaliya@CWDT011.CORP.CAPITAWORLD.COM>
2018-11-12 16:59:51 +0530

    removed entitymanager code

--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundprovider/impl/ProductMasterServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundprovider/impl/ProductMasterServiceImpl.java
@@ -8,6 +8,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
