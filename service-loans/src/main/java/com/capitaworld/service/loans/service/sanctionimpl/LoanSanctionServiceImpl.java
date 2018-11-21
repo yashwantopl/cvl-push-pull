4671e92219fa382a635457c08a0dcee1bda0cf2f
ankit.gupta <ankit.gupta@CWDT042.CORP.CAPITAWORLD.COM>
2018-11-21 19:39:04 +0530

    changes

--- service-loans/src/main/java/com/capitaworld/service/loans/service/sanctionimpl/LoanSanctionServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/sanctionimpl/LoanSanctionServiceImpl.java
@@ -243,8 +243,9 @@ public class LoanSanctionServiceImpl implements LoanSanctionService {
 							generateTokenRequest.setToken(token);
 							//Getting sanction and disbursement Details from Bank 
 							String encryptedString = sidbiIntegrationClient.getSanctionAndDisbursmentDetailList(token, generateTokenRequest.getBankToken() , userOrganisationRequest.getCodeLanguage());
+							String resJosn = null ;
 							if(!CommonUtils.isObjectNullOrEmpty(encryptedString)) {
-								String resJosn = sanctionAndDisbursementValidation(encryptedString , userOrganisationRequest.getCodeLanguage());
+								 resJosn = sanctionAndDisbursementValidation(encryptedString , userOrganisationRequest.getCodeLanguage());
 								try {
 									
 									sidbiIntegrationClient.setTokenAsExpired(generateTokenRequest, generateTokenRequest.getBankToken(), userOrganisationRequest.getCodeLanguage());
@@ -289,19 +290,24 @@ public class LoanSanctionServiceImpl implements LoanSanctionService {
 											//e.printStackTrace();
 										}
 									}
+									auditComponentBankToCW.saveBankToCWReqRes(resJosn , null , null , null , failureReason, userOrganisationRequest.getUserOrgId(), null);
 								}else {
 									//logger.info("*******Unable to store sanction or disbursement detail   **********  reasion is =={}", (res != null ? res.toString() : res));
 									 failureReason = "---------------Unable to update  sanction or disbursement detail to bank Side Response ----------------" + resJosn ;
+									auditComponentBankToCW.saveBankToCWReqRes(null , null, null, null, failureReason, userOrganisationRequest.getUserOrgId(), null);
 								}
+								
 							}else {
 								//logger.info("*******Null in getting sanction or disbursement detail  from  bank side  **********  reasion is ==> ", encryptedString +" org id ==> " + userOrganisationRequest.getUserOrgId() );
 								 failureReason ="----------------Null in getting sanction or disbursement detail  from  bank side  ------------------ org id ==> " + userOrganisationRequest.getUserOrgId();
-								
+								 auditComponentBankToCW.saveBankToCWReqRes(encryptedString , null, null, null, failureReason, userOrganisationRequest.getUserOrgId(), null);
 							}
-							auditComponentBankToCW.saveBankToCWReqRes(encryptedString, null, null, null, failureReason, userOrganisationRequest.getUserOrgId(), null);
+							
+							//auditComponentBankToCW.saveBankToCWReqRes(resJosn == null ? encryptedString : resJosn  , null , null , null , failureReason, userOrganisationRequest.getUserOrgId(), null);
+							
 						}catch(Exception e) {
 							e.printStackTrace();
-							logger.error("Error while Calling get token API");
+							logger.error("Error while Calling get token API and getting sanction & disbursemet detail MSG ==> "+e.getMessage());
 						}
 					}
 				}
