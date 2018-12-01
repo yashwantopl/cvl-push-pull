dca6f9e3c1b7b3ee4be73df95808b8e95b72b031
VinitaCW <vinita.choudhary@capitaworld.com>
2018-12-01 19:03:46 +0530

    change field name

--- service-loans/src/main/java/com/capitaworld/service/loans/repository/fundseeker/corporate/ApplicationProposalMappingRepository.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/repository/fundseeker/corporate/ApplicationProposalMappingRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface ApplicationProposalMappingRepository extends JpaRepository<ApplicationProposalMapping, Long> {
 
-    @Query("select count(proposal_mapping_id) from ApplicationProposalMapping apm where apm.proposalId =:proposalId and apm.isFinalLocked=1 and apm.isActive = true")
+    @Query("select count(proposalId) from ApplicationProposalMapping apm where apm.proposalId =:proposalId and apm.isFinalLocked=1 and apm.isActive = true")
     Long checkFinalDetailIsLocked(@Param("proposalId") Long proposalId);
 
     @Query("select count(proposalId) from ApplicationProposalMapping apm where apm.proposalId =:proposalId and apm.isPrimaryLocked=1 and apm.isActive = true")
