d7d1546b579a8e1a221916f9505c970901e33516
HirenCW <hiren@capitaworld.com>
2018-12-19 17:40:33 +0530

    update

--- service-loans/src/main/java/com/capitaworld/service/loans/controller/fundseeker/corporate/CamReportPdfDetailsController.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/controller/fundseeker/corporate/CamReportPdfDetailsController.java
@@ -34,6 +34,8 @@ public class CamReportPdfDetailsController {
 	@Autowired
 	private CamReportPdfDetailsService camReportPdfDetailsService;
 	
+	@Autowired
+	private InEligibleProposalCamReportService inEligibleProposalCamReportService;
 	
 	@Autowired
 	private ReportsClient reportsClient;
