5abf0a894d965ecf280b758a4b681d357f2b96b6
akshay <akshay.patel@capitaworld.com>
2018-12-19 19:47:01 +0530

    changes

--- service-loans/src/main/java/com/capitaworld/service/loans/domain/fundseeker/corporate/PrimaryCorporateDetail.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/domain/fundseeker/corporate/PrimaryCorporateDetail.java
@@ -96,24 +96,12 @@ public class PrimaryCorporateDetail extends LoanApplicationMaster implements Ser
     private Double incrementalMargin;
     
     
-    // Start By Akshay for OnePager Eligibility
-    @Column(name = "turn_over_prev_fin_year")
-    private Double turnOverPrevFinYear;
+    //By Akshay for OnePager Eligibility
+    @Column(name = "actual_sales_finYear_current")
+    private Double actualSalesFinYearCurrent;
     
-    @Column(name = "turn_over_curr_fin_year_till_month")
-    private Double turnOverCurrFinYearTillMonth;
-    
-    @Column(name = "projected_turn_over_curr_fin_year")
-    private Double projectedTurnOverCurrFinYear;
-    
-    @Column(name = "profit_curr_fin_year")
-    private Double profitCurrFinYear;
-    
-    @Column(name = "projected_profit_curr_fin_year")
-    private Double projectedProfitCurrFinYear;
-    
- // End By Akshay for OnePager Eligibility
-    
+    @Column(name = "estimated_sales_fin_year_next")
+    private Double estimatedSalesFinYearNext;
 
     public PrimaryCorporateDetail() {
     }
