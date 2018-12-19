57187730f27fa96c8601173367bc1879fe83c04e
daksh <daksh.logiciel@gmail.com>
2018-12-19 13:01:52 +0530

    Sonar Related Changes

--- service-loans/src/main/java/com/capitaworld/service/loans/service/common/impl/RecentViewServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/common/impl/RecentViewServiceImpl.java
@@ -7,6 +7,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -45,6 +47,8 @@ import com.capitaworld.service.users.model.UserResponse;
  */
 @Service
 public class RecentViewServiceImpl implements RecentViewService{
+
+	private static final Logger logger = LoggerFactory.getLogger(RecentViewServiceImpl.class);
 	
 	@Autowired
 	private NotificationClient notificationClient;
@@ -77,8 +81,7 @@ public class RecentViewServiceImpl implements RecentViewService{
 		try {
 			notificationResponse =  notificationClient.getAllRecentViewNotificationByAppId(request);
 		} catch (NotificationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error(CommonUtils.EXCEPTION,e);
 		}
 		Map<String, List<SysNotifyResponse>> a = notificationResponse.getRecentViewResponse();
 		List<SysNotifyResponse> thisWeek = a.get("thisWeek");
@@ -220,8 +223,7 @@ public class RecentViewServiceImpl implements RecentViewService{
 		try {
 			notificationResponse =  notificationClient.getAllRecentViewNotificationByProdId(request);
 		} catch (NotificationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error(CommonUtils.EXCEPTION,e);
 		}
 		Map<String, List<SysNotifyResponse>> a = notificationResponse.getRecentViewResponse();
 		List<SysNotifyResponse> thisWeek = a.get("thisWeek");
@@ -649,8 +651,7 @@ public class RecentViewServiceImpl implements RecentViewService{
 		try {
 			notificationResponse =  notificationClient.getAllLatestRecentViewNotificationByAppId(request);
 		} catch (NotificationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error(CommonUtils.EXCEPTION,e);
 		}
 		List<SysNotifyResponse> recentView = notificationResponse.getSysNotification();
 		RecentProfileViewDetailResponse detailResponse = new RecentProfileViewDetailResponse();
@@ -707,8 +708,7 @@ public class RecentViewServiceImpl implements RecentViewService{
 		try {
 			notificationResponse =  notificationClient.getAllLatestRecentViewNotificationByProdId(request);
 		} catch (NotificationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error(CommonUtils.EXCEPTION,e);
 		}
 		List<SysNotifyResponse> thisWeek = notificationResponse.getSysNotification();
 		RecentProfileViewDetailResponse detailResponse = new RecentProfileViewDetailResponse();
