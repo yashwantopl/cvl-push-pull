198c2e80c2e04d961f934d92f6a2db2117b3e6cb
daksh <daksh.logiciel@gmail.com>
2018-12-25 15:48:24 +0530

    Sonar Related Changes

--- api-loans/src/main/java/com/capitaworld/service/loans/utils/CommonUtils.java
+++ api-loans/src/main/java/com/capitaworld/service/loans/utils/CommonUtils.java
@@ -70,6 +70,7 @@ public class CommonUtils {
 	public static final String FALSE_LITERAL = "false";
 	public static final String NA = "NA";
 	public static final String AUDITED = "Audited";
+	public static final String PROJECTED = "Projected";
 	public static final String SUCCESS_RESULT = "Success Result";
 	public static final String DATA_FOUND = "Data Found.";
 	public static final String DATA_NOT_FOUND = "Data Not Found.";
@@ -83,6 +84,7 @@ public class CommonUtils {
 	public static final String OBLIGATION_MUST_BE_LESS_THAN_INCOME = "Obligation Must be less than Income";
 	public static final String REQUEST_DATA_NULL_OR_EMPTY = "Request Data Null Or Empty !!";
 	public static final String INTERNAL_SERVER_ERROR  = "Internal Server Error";
+	public static final String INSERT_TEXT_HERE = "Insert Text Here";
 	public static final String MANDATORY_FIELDS_MUST_NOT_BE_NULL = "Mandatory Fields Must Not be Null";
 	public static final String REQUESTED_DATA_CAN_NOT_BE_EMPTY = "Requested data can not be empty.";
 	public static final String INVALID_DATA_OR_REQUESTED_DATA_NOT_FOUND = "Invalid data or Requested data not found.";
