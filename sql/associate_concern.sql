74842bd349d5697b3ec55f48f8d23c818785bd5b
akshay <akshay.patel@capitaworld.com>
2018-12-26 19:55:23 +0530

    Added three column

---
+++ sql/associate_concern.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `loan_application`.`fs_corporate_associated_concern_details` ADD COLUMN pan VARCHAR(10);
+ALTER TABLE `loan_application`.`fs_corporate_associated_concern_details` ADD COLUMN financial_institution_name VARCHAR(200);
+ALTER TABLE `loan_application`.`fs_corporate_associated_concern_details` ADD COLUMN limit_availed VARCHAR(20);
