10e0c127d0813a91da9a1bf266a039bbc0b788eb
akshay <akshay.patel@capitaworld.com>
2019-01-01 20:14:19 +0530

    Uniform Product related changes

--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/impl/FundSeekerInputRequestServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/impl/FundSeekerInputRequestServiceImpl.java
@@ -652,6 +652,7 @@ public class FundSeekerInputRequestServiceImpl implements FundSeekerInputRequest
 		// ==== Director details
 		List<DirectorBackgroundDetailRequest> directorBackgroundDetailRequestList = fundSeekerInputRequest.getDirectorBackgroundDetailRequestsList();
 		try {
+			directorBackgroundDetailsService.inactive(fundSeekerInputRequest.getApplicationId(), fundSeekerInputRequest.getUserId());
 			for (DirectorBackgroundDetailRequest reqObj : directorBackgroundDetailRequestList) {
 				directorBackgroundDetailsService.saveDirectorInfo(reqObj, fundSeekerInputRequest.getApplicationId(), fundSeekerInputRequest.getUserId());
 			}
@@ -764,16 +765,28 @@ public class FundSeekerInputRequestServiceImpl implements FundSeekerInputRequest
 	@Override
 	public LoansResponse verifyGST(String gstin,Long applicationId,Long userId,MultipartFile[] uploadedFiles) {
 		//Uploading GST Receipt
-		boolean isDocumentUploaded = false;
 		DocumentRequest documentRequest = new DocumentRequest();
+		documentRequest.setApplicationId(applicationId);
+		documentRequest.setProductDocumentMappingId(DocumentAlias.GST_RECEIPT);
+		documentRequest.setUserType(DocumentAlias.UERT_TYPE_APPLICANT);
 		try{
-			documentRequest.setApplicationId(applicationId);
-			documentRequest.setProductDocumentMappingId(DocumentAlias.GST_RECEIPT);
-			documentRequest.setUserType(DocumentAlias.UERT_TYPE_APPLICANT);
-			
+			DocumentResponse documentResponse = dMSClient.deleteProductDocumentFromApplicationId(MultipleJSONObjectHelper.getStringfromObject(documentRequest));
+			if (CibilUtils.isObjectListNull(documentResponse)) {
+				logger.warn("Something goes wrong while Deleting GST Receipt for ApplicationId as Response Found Null===>{}",applicationId);
+			} else if (documentResponse.getStatus() == HttpStatus.OK.value()) {
+				logger.info("Successfully Deleting GST Receipt For Application Id==>{}",applicationId);
+			} else {
+				logger.warn("Something goes wrong while Deleting GST Receipt for ApplicationId===>{}",applicationId);
+			}
+		}catch(Exception e){
+			logger.error("Error while Deleting Existing Doccuments of GST and ITR : {}", e);
+		}
+		
+		
+		boolean isDocumentUploaded = false;
+		try{
 			for (MultipartFile uploadedFile : uploadedFiles) {
 				if(CommonUtils.isObjectNullOrEmpty(uploadedFile)) {
-					/*logger.info("CURRENT MULTIPART OBJECT IS NULL OR EMPTY");*/
 					continue;
 				}
 				documentRequest.setOriginalFileName(uploadedFile.getOriginalFilename());
