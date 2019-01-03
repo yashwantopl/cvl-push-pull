025394cc68235fa4e175aeeda4e050e9d07706f2
KushalCW <kushal@capitaworld.com>
2019-01-03 12:08:05 +0530

    update

--- service-loans/src/main/java/com/capitaworld/service/loans/domain/IndustrySectorDetail.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/domain/IndustrySectorDetail.java
@@ -52,6 +52,7 @@ public class IndustrySectorDetail implements Serializable {
 	private Long sectorId;
 
 	public IndustrySectorDetail() {
+		// Do nothing because of X and Y.
 	}
 
 	public Long getId() {
