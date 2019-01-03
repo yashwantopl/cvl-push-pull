b6ae98669aeca8af4d4a0c2e361c6cf04a864b42
akshay <akshay.patel@capitaworld.com>
2019-01-03 21:08:21 +0530

    Uniform Product Related changes

---
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundprovider/impl/UniformProductParameterAuditServiceImpl.java
@@ -0,0 +1,50 @@
+package com.capitaworld.service.loans.service.fundprovider.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.capitaworld.service.loans.domain.fundprovider.UniformProductParamterAudit;
+import com.capitaworld.service.loans.model.corporate.UniformProductParamterRequest;
+import com.capitaworld.service.loans.repository.fundprovider.UniformProductParameterAuditRepository;
+import com.capitaworld.service.loans.service.fundprovider.UniformProductParameterAuditService;
+import com.capitaworld.service.loans.utils.CommonUtils;
+
+@Service
+public class UniformProductParameterAuditServiceImpl implements UniformProductParameterAuditService {
+	private static final Logger logger = LoggerFactory.getLogger(UniformProductParameterAuditServiceImpl.class);
+
+	@Autowired
+	private UniformProductParameterAuditRepository uniformProductParameterAuditRepository; 
+
+
+	@Override
+	@Transactional
+	public Boolean saveOrUpdate(UniformProductParamterRequest productParamterRequest) {
+		return true;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<UniformProductParamterRequest> get(Long userId, Long orgId) {
+		List<UniformProductParamterAudit> uniformProductParamters = uniformProductParameterAuditRepository
+				.findByOrgId(orgId);
+		if (CommonUtils.isObjectListNull(uniformProductParamters)) {
+			return null;
+		}
+		List<UniformProductParamterRequest> paramterRequests = new ArrayList<>(uniformProductParamters.size());
+		UniformProductParamterRequest paramterRequest = null;
+		for(UniformProductParamterAudit uniformProductParamter : uniformProductParamters){
+			paramterRequest = new UniformProductParamterRequest();
+			BeanUtils.copyProperties(uniformProductParamter, paramterRequest);
+			paramterRequests.add(paramterRequest);
+		}
+		return paramterRequests;
+	}
+}
