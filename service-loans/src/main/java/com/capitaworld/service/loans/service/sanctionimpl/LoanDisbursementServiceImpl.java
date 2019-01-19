8f0d9f0cf2d49647a40de23698e9115426b419c2
HarshitCW <harshit.suhagiya@capitaworld.com>
2019-01-19 15:43:54 +0530

    - add modified date

--- service-loans/src/main/java/com/capitaworld/service/loans/service/sanctionimpl/LoanDisbursementServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/sanctionimpl/LoanDisbursementServiceImpl.java
@@ -164,6 +164,11 @@ public class LoanDisbursementServiceImpl implements LoanDisbursementService {
 						loanDisbursementRequest.setOrgId(orgId);
 						IneligibleProposalDetails ineligibleProposalDetails = (IneligibleProposalDetails) offlineProcessedAppRepository.findByAppliationId(loanDisbursementRequest.getApplicationId(),orgId);
 						ineligibleProposalDetails.setIsDisbursed(true);
+						ineligibleProposalDetails.setModifiedDate(new Date());
+						if(!CommonUtils.isObjectNullOrEmpty(loanDisbursementRequest.getActionBy())) {
+							ineligibleProposalDetails.setModifiedBy(Long.valueOf(loanDisbursementRequest.getActionBy()));
+						}
+						ineligibleProposalDetails.setStatus(CommonUtils.InEligibleProposalStatus.DISBURED);
 						LoanSanctionDomain loanSanctionDomainOld =loanSanctionRepository.findByAppliationId(loanDisbursementRequest.getApplicationId());
 						loanSanctionDomainOld.setIsPartiallyDisbursedOffline(true);
 					}
