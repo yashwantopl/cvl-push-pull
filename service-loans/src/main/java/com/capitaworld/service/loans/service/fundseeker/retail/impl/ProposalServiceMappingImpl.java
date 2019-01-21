52854b4cd35353042e923e65d678e1a2615697cf
VinitaCW <vinita.choudhary@capitaworld.com>
2019-01-21 11:08:00 +0530

    Multiple bank changes

--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/retail/impl/ProposalServiceMappingImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/retail/impl/ProposalServiceMappingImpl.java
@@ -1448,11 +1448,11 @@ public class ProposalServiceMappingImpl implements ProposalService {
 				if(!proposalDetails.getUserOrgId().toString().equals(userOrgId))
 				{
 					if(ProposalStatus.APPROVED ==  proposalDetails.getProposalStatusId().getId())
-						messageOfButton="Proposal is Sanctioned";
+						messageOfButton="This proposal has been Sanctioned by Other Bank.";
 					else if(ProposalStatus.DISBURSED ==  proposalDetails.getProposalStatusId().getId())
-						messageOfButton="Proposal is Disbursed";
+						messageOfButton="This proposal has been Disbursed by Other Bank.";
 					else if(ProposalStatus.PARTIALLY_DISBURSED ==  proposalDetails.getProposalStatusId().getId())
-						messageOfButton="Proposal is Partially Disbursed";
+						messageOfButton="This proposal has been Partially Disbursed by Other Bank.";
 					isButtonDisplay=false;
 
 					proposalMappingRequest.setMessageOfButton(messageOfButton);
