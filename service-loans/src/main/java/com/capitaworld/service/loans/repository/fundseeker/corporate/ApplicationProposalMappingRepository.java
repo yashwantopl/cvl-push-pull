d1f71705b94f6466a6b943d7f491a8da9e40089b
VinitaCW <vinita.choudhary@capitaworld.com>
2019-02-05 12:53:54 +0530

    Removed multiple bank related class

--- service-loans/src/main/java/com/capitaworld/service/loans/repository/fundseeker/corporate/ApplicationProposalMappingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.capitaworld.service.loans.repository.fundseeker.corporate;
-
-import com.capitaworld.service.loans.domain.fundseeker.ApplicationProposalMapping;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationProposalMappingRepository extends JpaRepository<ApplicationProposalMapping, Long> {
-
-}
--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/ApplicationProposalMappingService.java
+++
@@ -1,8 +0,0 @@
-package com.capitaworld.service.loans.service.fundseeker.corporate;
-
-import com.sun.org.apache.xpath.internal.operations.Bool;
-
-public interface ApplicationProposalMappingService {
-
-    Boolean saveOrUpdate();
-}
