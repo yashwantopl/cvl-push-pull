1f32481b33ad172f846d2217be02b5e3153378ff
ankit.gupta <ankit.gupta@CWDT042.CORP.CAPITAWORLD.COM>
2019-04-24 19:26:03 +0530

    remove client

--- service-loans/src/main/java/com/capitaworld/service/loans/boot/LoansMain.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/boot/LoansMain.java
@@ -226,12 +226,12 @@ public class LoansMain {
 		return thirdPartyClient;
 	}
 
-	@Bean
+	/*@Bean
 	public SidbiIntegrationClient sidbiIntegrationClient() {
 		SidbiIntegrationClient sidbiIntegrationClient = new SidbiIntegrationClient();
 		applicationContext.getAutowireCapableBeanFactory().autowireBean(sidbiIntegrationClient);
 		return sidbiIntegrationClient;
-	}
+	}*/
 
 	@Bean
 	public McaClient mcaClient() {
