4dab1b17d698584f885e4eeee9157b9aa4379816
Kishan Jadav <kishan.jadav@capitaworld.com>
2019-04-30 12:19:06 +0530

    Add Category in Primary view

--- api-loans/src/main/java/com/capitaworld/service/loans/model/teaser/primaryview/CorporatePrimaryViewResponse.java
+++ api-loans/src/main/java/com/capitaworld/service/loans/model/teaser/primaryview/CorporatePrimaryViewResponse.java
@@ -46,6 +46,7 @@ public class CorporatePrimaryViewResponse {
     private String collateralSecurityAmount;
     private List<Object> profilePic;
     private Double enhancementAmount;
+    private String castCategory;
 
     /*private String  isBusinessAssetChecked;
     private String  isWorkingCapitalChecked;
@@ -1245,6 +1246,12 @@ public class CorporatePrimaryViewResponse {
 	}
 	public void setCollateralSecurityDetails(List<CollateralSecurityDetailRequest> collateralSecurityDetails) {
 		this.collateralSecurityDetails = collateralSecurityDetails;
+	}
+	public String getCastCategory() {
+		return castCategory;
+	}
+	public void setCastCategory(String castCategory) {
+		this.castCategory = castCategory;
 	}
 	
 	
