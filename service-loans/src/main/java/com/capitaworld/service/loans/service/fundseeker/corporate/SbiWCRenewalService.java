953806ad9b326b0473cda28faf492fc6faae166c
DhavalCW <dhaval@capitaworld.com>
2019-05-01 17:03:53 +0530

    sbi renewal issue solved

--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/SbiWCRenewalService.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/SbiWCRenewalService.java
@@ -4,7 +4,7 @@ package com.capitaworld.service.loans.service.fundseeker.corporate;
  * Created by dhaval.panchal on 25-Apr-19.
  */
 public interface SbiWCRenewalService {
-    public boolean callSkipPayment(Long application_id);
+    public boolean callSkipPayment(Long application_id, Long userId);
 
     public boolean callMatchEngine(Long application_id, Long userId);
 }
