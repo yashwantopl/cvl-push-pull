2c5d012fe4f705fb19e152ecd860e9b8a3208fd4
nilay.darji <nilay.darji@CWDT016.CORP.CAPITAWORLD.COM>
2019-05-04 12:35:30 +0530

    cibil changes

--- service-loans/src/main/java/com/capitaworld/service/loans/service/teaser/finalview/impl/CorporateFinalViewServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/teaser/finalview/impl/CorporateFinalViewServiceImpl.java
@@ -364,6 +364,7 @@ public class CorporateFinalViewServiceImpl implements CorporateFinalViewService 
 		CorporateApplicantDetail corporateApplicantDetail = corporateApplicantDetailRepository
 				.getByApplicationAndProposalIdAndUserId(userId, toApplicationId,proposalMapId); // NEW BASED ON PROPOSAL MAP ID
 
+		corporateFinalViewResponse.setComercialPanNo(corporateApplicantDetail.getPanNo()); 
 		// SET VALUE TO RESPONSE
 		if (corporateApplicantDetail != null) {
 			BeanUtils.copyProperties(corporateApplicantDetail, corporateFinalViewResponse);
