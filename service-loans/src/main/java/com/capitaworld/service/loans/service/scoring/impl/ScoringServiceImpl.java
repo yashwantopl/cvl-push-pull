175c61a8d481fce0592dc890c9957044e2cda120
akshay <akshay.patel@capitaworld.com>
2019-05-09 11:29:32 +0530

    Changesl

--- service-loans/src/main/java/com/capitaworld/service/loans/service/scoring/impl/ScoringServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/scoring/impl/ScoringServiceImpl.java
@@ -58,6 +58,7 @@ import com.capitaworld.service.loans.domain.fundseeker.corporate.DirectorBackgro
 import com.capitaworld.service.loans.domain.fundseeker.corporate.LiabilitiesDetails;
 import com.capitaworld.service.loans.domain.fundseeker.corporate.OperatingStatementDetails;
 import com.capitaworld.service.loans.domain.fundseeker.corporate.PrimaryCorporateDetail;
+import com.capitaworld.service.loans.domain.fundseeker.retail.PrimaryHomeLoanDetail;
 import com.capitaworld.service.loans.domain.fundseeker.retail.RetailApplicantDetail;
 import com.capitaworld.service.loans.exceptions.LoansException;
 import com.capitaworld.service.loans.model.LoansResponse;
@@ -78,6 +79,7 @@ import com.capitaworld.service.loans.repository.fundseeker.corporate.PrimaryCorp
 import com.capitaworld.service.loans.repository.fundseeker.corporate.ProfitibilityStatementDetailRepository;
 import com.capitaworld.service.loans.repository.fundseeker.retail.BankingRelationlRepository;
 import com.capitaworld.service.loans.repository.fundseeker.retail.CoApplicantDetailRepository;
+import com.capitaworld.service.loans.repository.fundseeker.retail.PrimaryHomeLoanDetailRepository;
 import com.capitaworld.service.loans.repository.fundseeker.retail.RetailApplicantDetailRepository;
 import com.capitaworld.service.loans.repository.fundseeker.retail.RetailApplicantIncomeRepository;
 import com.capitaworld.service.loans.service.fundseeker.corporate.FinancialArrangementDetailsService;
@@ -207,6 +209,9 @@ public class ScoringServiceImpl implements ScoringService {
 
     @Autowired
     private ScoringRequestDetailRepository scoringRequestDetailRepository;
+    
+    @Autowired
+    private PrimaryHomeLoanDetailRepository primaryHomeLoanDetailRepository; 
 
     @Autowired
     private EligibilityClient eligibilityClient;
@@ -1236,6 +1241,7 @@ public class ScoringServiceImpl implements ScoringService {
         List<Long> coAppITRUploadedIds = null;
         Double netMonthlyIncome = 0.0d;
         Double grossAnnualIncome = 0.0d;
+        PrimaryHomeLoanDetail primaryHomeLoanDetail = null;
 
         if(!CommonUtils.isListNullOrEmpty(scoringRequestLoansList)) {
         	applicationId = scoringRequestLoansList.get(0).getApplicationId();
@@ -1243,6 +1249,12 @@ public class ScoringServiceImpl implements ScoringService {
         	if (CommonUtils.isObjectNullOrEmpty(retailApplicantDetail)) {
                 logger.error(ERROR_WHILE_GETTING_RETAIL_APPLICANT_DETAIL_FOR_PERSONAL_LOAN_SCORING);
                 return new ResponseEntity<>(new LoansResponse(ERROR_WHILE_GETTING_RETAIL_APPLICANT_DETAIL_FOR_HOME_LOAN_SCORING, HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.OK);
+            }
+        	
+        	primaryHomeLoanDetail = primaryHomeLoanDetailRepository.getByApplication(applicationId);
+        	if (CommonUtils.isObjectNullOrEmpty(primaryHomeLoanDetail)) {
+                logger.error(ERROR_WHILE_GETTING_RETAIL_APPLICANT_DETAIL_FOR_PERSONAL_LOAN_SCORING);
+                return new ResponseEntity<>(new LoansResponse("Primary Detail Must Not be null While Calculating Home Loan Scoring", HttpStatus.INTERNAL_SERVER_ERROR.value()), HttpStatus.OK);
             }
         	EligibilityResponse eligibilityResponse = null;
 			try {
@@ -1537,6 +1549,9 @@ public class ScoringServiceImpl implements ScoringService {
             			case ScoreParameter.Retail.HomeLoan.REPAYMENT_PERIOD:
             				break;
             			case ScoreParameter.Retail.HomeLoan.AGE_PROPERTY:
+	            				if(primaryHomeLoanDetail.getOldPropYear() != null) {
+	            					
+	            				}
             				break;
             			case ScoreParameter.Retail.HomeLoan.AVG_DEPOS_LAST_6_MONTH:
             				break;
