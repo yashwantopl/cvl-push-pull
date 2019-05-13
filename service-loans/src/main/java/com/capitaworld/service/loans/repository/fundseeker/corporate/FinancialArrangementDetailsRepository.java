80d33d168ad422b6368ac868d49553f61ba7d8a3
akshay <akshay.patel@capitaworld.com>
2019-05-13 17:37:25 +0530

    Total EMI Related Changes

--- service-loans/src/main/java/com/capitaworld/service/loans/repository/fundseeker/corporate/FinancialArrangementDetailsRepository.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/repository/fundseeker/corporate/FinancialArrangementDetailsRepository.java
@@ -75,7 +75,7 @@ public interface FinancialArrangementDetailsRepository extends JpaRepository<Fin
 	@Query("select sum(o.emi) from FinancialArrangementsDetail o where o.applicationId.id =:applicationId and o.directorBackgroundDetail.id =:directorId and o.isActive = true and o.directorBackgroundDetail IS NULL")
 	public Double getTotalEmiByApplicationIdAndDirectorId(@Param("applicationId")Long applicationId,@Param("directorId")Long directorId);
 
-	@Query("select sum(o.emi) from FinancialArrangementsDetail o where o.applicationId.id =:applicationId and o.directorBackgroundDetail IS NOT NULL and o.isActive = true and LOWER(o.loanType) NOT IN (:loanType) and o.outstandingAmount IS NOT NULL and o.outstandingAmount > 0 and o.directorBackgroundDetail IS NULL")
+	@Query("select sum(o.emi) from FinancialArrangementsDetail o where o.applicationId.id =:applicationId and o.directorBackgroundDetail IS NOT NULL and o.isActive = true and LOWER(o.loanType) NOT IN (:loanType) and o.outstandingAmount IS NOT NULL and o.outstandingAmount > 0")
 	public Double getTotalEmiOfAllDirByApplicationId(@Param("applicationId")Long applicationId,@Param("loanType") List<String> loanType);
 	
 	@Query("select sum(o.emi) from FinancialArrangementsDetail o where o.applicationId.id =:applicationId and o.directorBackgroundDetail IS NULL and o.isActive = true and LOWER(o.loanType) NOT IN (:loanType) and o.outstandingAmount IS NOT NULL and o.outstandingAmount > 0")
