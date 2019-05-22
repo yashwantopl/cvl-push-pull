af11981a679733ec91fd8a49cbe2be300f24133d
SanketCW <sanket.devare@capitaworld.com>
2019-05-22 18:32:04 +0530

    Changes done for epfo integration

---
+++ service-loans/src/main/java/com/capitaworld/service/loans/utils/EPFOAsyncComponent.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package com.capitaworld.service.loans.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import com.capitaworld.api.ekyc.model.epf.request.EmployerDefaulterRequest;
+import com.capitaworld.api.ekyc.model.epf.request.EmployerRequest;
+import com.capitaworld.api.ekyc.model.epf.request.EmployerVerificationRequest;
+import com.capitaworld.client.ekyc.EPFClient;
+
+/**
+ * @author sanket
+ *
+ */
+@Component
+public class EPFOAsyncComponent {
+	
+	@Autowired
+	private EPFClient epfClient;
+	
+		
+	@Async
+	public void callAPI(EmployerRequest employerRequest) {
+		try {
+		// Defaulters Call
+		EmployerRequest req= new EmployerRequest();
+		req.setEmployerDefaulterRequest(new EmployerDefaulterRequest(employerRequest.getEmployerDefaulterRequest().getKid()));
+		callAllAPIForData(req);
+		
+		// Employment Verification
+		EmployerRequest reqEmpVer= new EmployerRequest();
+		reqEmpVer.setEmployerVerificationRequest(new EmployerVerificationRequest(null,employerRequest.getEmployerVerificationRequest().getEmployerName(), employerRequest.getEmployerVerificationRequest().getEmployeeName(),employerRequest.getEmployerVerificationRequest().getMobile(),employerRequest.getEmployerVerificationRequest().getEmailId()));
+		callAllAPIForData(reqEmpVer);
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+	
+	
+	
+	private void callAllAPIForData(EmployerRequest employerRequest) {
+		try {
+			epfClient.getVerifyEmployment(employerRequest);
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+		
+		
+	}
+
+}
