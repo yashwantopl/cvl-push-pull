d82fa9b5c818734fc49cae4a9afe73571f720ce2
kushal shah <kushal.shah@onlinepsbloans.com>
2019-06-28 18:59:58 +0530

    update

--- service-loans/src/main/java/com/capitaworld/service/loans/config/CorsFilter.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/config/CorsFilter.java
@@ -14,13 +14,13 @@ public class CorsFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "*");
-		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Max-Age", "-1");
 		response.setHeader("Access-Control-Allow-Headers",
 			    "Content-Type, Access-Control-Allow-Headers, X-Requested-With,tk_ac,ur_cu,tk_rc,tk_lg,req_auth,*");
 		response.setHeader("X-Frame-Options","ALLOW-FROM *.capitaworld.com");
 		HttpServletRequest request = ((HttpServletRequest) req);
 		if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
-			response.setStatus(HttpServletResponse.SC_OK);
+			response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
 		} else {
 			chain.doFilter(req, res);
 		}
