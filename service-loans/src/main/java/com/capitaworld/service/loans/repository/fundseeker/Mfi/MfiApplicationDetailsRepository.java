b8bc8fc428a845a4557f8e29d7cb6ac5d31855cb
CWAmit <amit.makwana@onlinepsbloans.com>
2019-07-24 16:19:54 +0530

    MFI changes after merge

--- service-loans/src/main/java/com/capitaworld/service/loans/repository/fundseeker/Mfi/MfiApplicationDetailsRepository.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/repository/fundseeker/Mfi/MfiApplicationDetailsRepository.java
@@ -14,11 +14,9 @@ import java.util.List;
 
 public interface MfiApplicationDetailsRepository extends JpaRepository<MFIApplicantDetail,Long> {
 
-    @Query("select fn.applicationId.id,fn.firstName,fn.lastName,fn.middleName,fn.birthDate,fn.genderId,fn.mobile,fn.email,fn.addressSameAsAadhar,fn.currentDistrict,fn.aadharDistrict,fn.currentHouse,fn.aadharHouse,fn.currentLandmark,fn.aadharLandmark,fn.currentLocation,fn.aadharLocation,fn.currentState,fn.aadharState,fn.currentStreet,fn.aadharStreet,fn.currentVtc,fn.aadharVtc,fn.aadharSubdist,fn.currentSubdist,fn.aadharPo,fn.currentPo,fn.aadharCareOf,fn.addressPincode,fn.aadharPincode,fn.addressProofType from MFIApplicantDetail fn where fn.applicationId = :appId and fn.isActive = true")
-    public List findAadharDetailsByAppId(@Param("appId") Long appId);
-
     @Query("from MFIApplicantDetail fn where fn.applicationId.id = :appId and fn.type= :type and fn.isActive = true")
     public MFIApplicantDetail findByAppIdAndType(@Param("appId") Long appId,@Param("type") Integer type);
+
     @Query("select new com.capitaworld.service.loans.model.micro_finance.AadharDetailsReq(fn.applicationId.id,fn.firstName,fn.lastName,fn.middleName,fn.birthDate,fn.genderId,fn.mobile,fn.email,fn.addressSameAsAadhar,fn.currentDistrict,fn.aadharDistrict,fn.currentHouse,fn.aadharHouse,fn.currentLandmark,fn.aadharLandmark,fn.currentLocation,fn.aadharLocation,fn.currentState,fn.aadharState,fn.currentStreet,fn.aadharStreet,fn.currentVtc,fn.aadharVtc,fn.aadharSubdist,fn.currentSubdist,fn.aadharPo,fn.currentPo,fn.aadharCareOf,fn.addressPincode,fn.aadharPincode,fn.addressProofType) from MFIApplicantDetail fn where fn.applicationId.id = :appId and fn.isActive = true")
     public List<AadharDetailsReq> findAadharDetailsByAppId(@Param("appId") Long appId);
 
