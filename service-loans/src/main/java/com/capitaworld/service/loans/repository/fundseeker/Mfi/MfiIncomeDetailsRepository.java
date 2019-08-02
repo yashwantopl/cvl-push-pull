4bb00d0754013496a3fd4db4f6e582dcb062334c
HirenCW <hiren@capitaworld.com>
2019-08-02 14:41:06 +0530

    updated type in responce

--- service-loans/src/main/java/com/capitaworld/service/loans/repository/fundseeker/Mfi/MfiIncomeDetailsRepository.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/repository/fundseeker/Mfi/MfiIncomeDetailsRepository.java
@@ -20,7 +20,7 @@ import com.capitaworld.service.loans.model.micro_finance.MfiIncomeDetailsReq;
  */
 public interface MfiIncomeDetailsRepository extends JpaRepository<MfiIncomeDetails,Long> {
 	
-	@Query("select new com.capitaworld.service.loans.model.micro_finance.MfiIncomeDetailsReq(fn.applicationId,fn.occupation,fn.netIncome,fn.frequencyIncome,fn.monthlyIncome,fn.yearlyIncome,fn.relationId) from MfiIncomeDetails fn where fn.applicationId = :appId and fn.type =:type")
+	@Query("select new com.capitaworld.service.loans.model.micro_finance.MfiIncomeDetailsReq(fn.applicationId,fn.occupation,fn.netIncome,fn.frequencyIncome,fn.monthlyIncome,fn.yearlyIncome,fn.relationId,fn.type) from MfiIncomeDetails fn where fn.applicationId = :appId and fn.type =:type")
 	public List<MfiIncomeDetailsReq> findIncomeDetailsByAppId(@Param("appId") Long appId,@Param("type") Integer type);
 	
 }
