c470b4bfc3d1c4f57e313c57305e867fd1fecc93
JaiminCW <jaimin.darji@onlinepsbloans.com>
2019-08-05 17:36:01 +0530

    -- remaining fields added

--- api-loans/src/main/java/com/capitaworld/service/loans/model/micro_finance/MfiApplicantDetailsReq.java
+++ api-loans/src/main/java/com/capitaworld/service/loans/model/micro_finance/MfiApplicantDetailsReq.java
@@ -155,12 +155,19 @@ public class MfiApplicantDetailsReq {
 	private String nomineeLandmark;
 
 	private Integer academicReligion;
+
 	private Integer academicCaste;
+
 	private Integer isAcademicLifeInsurance;
+
 	private Integer houseOwnership;
+
 	private Integer areaType;
+
 	private Integer businessPremises;
+
 	private Integer expInSameLine;
+
 	private Double academicSumInsured;
 
 	private Integer type;
@@ -1670,39 +1677,6 @@ public class MfiApplicantDetailsReq {
 	public void setCompetition(Integer competition) {
 		this.competition = competition;
 	}
-
-	public void setAcademicReligion(Integer academicReligion) {
-		this.academicReligion = academicReligion;
-	}
-
-	public void setAcademicCaste(Integer academicCaste) {
-		this.academicCaste = academicCaste;
-	}
-
-	public void setIsAcademicLifeInsurance(Integer isAcademicLifeInsurance) {
-		this.isAcademicLifeInsurance = isAcademicLifeInsurance;
-	}
-
-	public void setHouseOwnership(Integer houseOwnership) {
-		this.houseOwnership = houseOwnership;
-	}
-
-	public void setAreaType(Integer areaType) {
-		this.areaType = areaType;
-	}
-
-	public void setBusinessPremises(Integer businessPremises) {
-		this.businessPremises = businessPremises;
-	}
-
-	public void setExpInSameLine(Integer expInSameLine) {
-		this.expInSameLine = expInSameLine;
-	}
-
-	public void setAcademicSumInsured(Double academicSumInsured) {
-		this.academicSumInsured = academicSumInsured;
-	}
-
 	public List<MfiIncomeDetailsReq> getIncomeDetailsTypeTwoList() {
 		return incomeDetailsTypeTwoList;
 	}
@@ -1717,5 +1691,69 @@ public class MfiApplicantDetailsReq {
 
 	public void setJobId(Long jobId) {
 		this.jobId = jobId;
+	}
+
+	public Integer getAcademicReligion() {
+		return academicReligion;
+	}
+
+	public void setAcademicReligion(Integer academicReligion) {
+		this.academicReligion = academicReligion;
+	}
+
+	public Integer getIsAcademicLifeInsurance() {
+		return isAcademicLifeInsurance;
+	}
+
+	public void setIsAcademicLifeInsurance(Integer isAcademicLifeInsurance) {
+		this.isAcademicLifeInsurance = isAcademicLifeInsurance;
+	}
+
+	public Integer getAcademicCaste() {
+		return academicCaste;
+	}
+
+	public void setAcademicCaste(Integer academicCaste) {
+		this.academicCaste = academicCaste;
+	}
+
+	public Integer getHouseOwnership() {
+		return houseOwnership;
+	}
+
+	public void setHouseOwnership(Integer houseOwnership) {
+		this.houseOwnership = houseOwnership;
+	}
+
+	public Integer getAreaType() {
+		return areaType;
+	}
+
+	public void setAreaType(Integer areaType) {
+		this.areaType = areaType;
+	}
+
+	public Integer getBusinessPremises() {
+		return businessPremises;
+	}
+
+	public void setBusinessPremises(Integer businessPremises) {
+		this.businessPremises = businessPremises;
+	}
+
+	public Integer getExpInSameLine() {
+		return expInSameLine;
+	}
+
+	public void setExpInSameLine(Integer expInSameLine) {
+		this.expInSameLine = expInSameLine;
+	}
+
+	public Double getAcademicSumInsured() {
+		return academicSumInsured;
+	}
+
+	public void setAcademicSumInsured(Double academicSumInsured) {
+		this.academicSumInsured = academicSumInsured;
 	}
 }
