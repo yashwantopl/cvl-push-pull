0815d243152b31268e543a581b41d99712c720ad
JaiminCW <jaimin.darji@onlinepsbloans.com>
2019-08-17 16:39:22 +0530

    -- id add in financial arrengement

--- service-loans/src/main/java/com/capitaworld/service/loans/repository/fundseeker/Mfi/MfiFinancialArrangementDetailsRepository.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/repository/fundseeker/Mfi/MfiFinancialArrangementDetailsRepository.java
@@ -22,7 +22,7 @@ public interface MfiFinancialArrangementDetailsRepository extends JpaRepository<
 	public int inActive(@Param("createdBy") Long createdBy, @Param("applicationId") Long applicationId, @Param("applicantId") Long applicantId);
 
 	
-	@Query("select new com.capitaworld.service.loans.model.mfi.MFIFinancialArrangementRequest(fn.applicationId.id,fn.financialInstitutionName,fn.otherInstitutionName,fn.amount,fn.loanDate,fn.reportedDate,fn.loanType,fn.emi,fn.createdBy,fn.createdDate,fn.modifiedBy,fn.modifiedDate,fn.outstandingAmount,fn.isManuallyAdded,fn.bureauOutstandingAmount,fn.bankerOutstandingAmount,fn.bureauOrCalculatedEmi,fn.isBureauEmi,fn.isActive,fn.provider,fn.applicantId) from MfiFinancialArrangementsDetail fn where fn.applicationId.id= :appId and fn.applicantId =:applicantId and fn.isActive=true")
+	@Query("select new com.capitaworld.service.loans.model.mfi.MFIFinancialArrangementRequest(fn.id,fn.applicationId.id,fn.financialInstitutionName,fn.otherInstitutionName,fn.amount,fn.loanDate,fn.reportedDate,fn.loanType,fn.emi,fn.createdBy,fn.createdDate,fn.modifiedBy,fn.modifiedDate,fn.outstandingAmount,fn.isManuallyAdded,fn.bureauOutstandingAmount,fn.bankerOutstandingAmount,fn.bureauOrCalculatedEmi,fn.isBureauEmi,fn.isActive,fn.provider,fn.applicantId) from MfiFinancialArrangementsDetail fn where fn.applicationId.id= :appId and fn.applicantId =:applicantId and fn.isActive=true")
     public List<MFIFinancialArrangementRequest> getFinancialDetailsByAppId(@Param("appId") Long appId,@Param("applicantId") Long applicantId);
 	
 }
