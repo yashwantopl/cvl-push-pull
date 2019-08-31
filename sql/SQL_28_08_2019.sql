966c91584387f12d09742f632d2401cd5715dede
Jaimin Darji <49337109+JaiminCW@users.noreply.github.com>
2019-08-31 12:33:35 +0530

    column added

--- sql/SQL_28_08_2019.sql
+++ sql/SQL_28_08_2019.sql
@@ -17,3 +17,6 @@ INSERT INTO document_management.product_document_mapping (id, document_id, is_ma
 Database : loan_application
 
 ALTER TABLE loan_application.fs_mfi_applicant_details ADD COLUMN dpn_doc VARCHAR(20) NULL AFTER total_emi, ADD COLUMN loi_doc VARCHAR(20) NULL AFTER dpn_doc, ADD COLUMN loh_doc VARCHAR(20) NULL AFTER loi_doc, ADD COLUMN agreement_doc VARCHAR(20) NULL AFTER loh_doc; 
+
+ALTER TABLE `loan_application`.`fs_mfi_bank_details` CHANGE `passbook_img` LONGTEXT NULL;
+ALTER TABLE `loan_application`.`fs_mfi_applicant_details`  ADD COLUMN `total_emi` DOUBLE NULL AFTER `total_emi`;
