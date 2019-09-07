234d90260b68a36de6c3ce72c5ea791b637f631f
Maaz Shaikh <maaz.shaikh@capitaworld.com>
2019-09-07 16:47:57 +0530

    added condition

--- service-loans/src/main/java/com/capitaworld/service/loans/config/FPAsyncComponent.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/config/FPAsyncComponent.java
@@ -966,8 +966,7 @@ public class FPAsyncComponent {
 						try {
 							logger.info("Calling One form client for getting state by state list Id");
 							OneFormResponse oneFormResponse = oneFormClient.getStateByStateListId(stateList);
-							List<Map<String, Object>> oneResponseDataList = (List<Map<String, Object>>) oneFormResponse
-									.getListData();
+							List<Map<String, Object>> oneResponseDataList = (List<Map<String, Object>>) oneFormResponse.getListData();
 							if (oneResponseDataList != null && !oneResponseDataList.isEmpty()) {
 								MasterResponse masterResponse = MultipleJSONObjectHelper
 										.getObjectFromMap(oneResponseDataList.get(0), MasterResponse.class);
@@ -987,7 +986,9 @@ public class FPAsyncComponent {
 
 			//	when proposal belongs to PL or HL
 			if(!CommonUtils.isObjectNullOrEmpty(applicationRequest)
-					&& (applicationRequest.getProductId() == CommonUtils.LoanType.PERSONAL_LOAN.getValue() || applicationRequest.getProductId() == CommonUtils.LoanType.HOME_LOAN.getValue())){
+				&& (applicationRequest.getProductId() == CommonUtils.LoanType.PERSONAL_LOAN.getValue() 
+				|| applicationRequest.getProductId() == CommonUtils.LoanType.HOME_LOAN.getValue()
+				|| applicationRequest.getProductId() == CommonUtils.LoanType.AUTO_LOAN.getValue())){
 				domainId = DomainValue.RETAIL.getId();
 				fsName=applicationRequest.getUserName();
 				address=applicationRequest.getAddress();
