c331b562c9082521f7254d9ceac9babc8d21b027
JaiminCW <jaimin.darji@onlinepsbloans.com>
2019-09-25 16:00:27 +0530

    -- bank relation issue resolve

--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/retail/impl/PlRetailApplicantServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/retail/impl/PlRetailApplicantServiceImpl.java
@@ -757,28 +757,28 @@ public class PlRetailApplicantServiceImpl implements PlRetailApplicantService {
             }
 
             BeanUtils.copyProperties(plRetailApplicantRequest, applicantDetail, CommonUtils.IgnorableCopy.getPlRetailProfile());
+//
+//            List<BankingRelation> bankingRelations = new ArrayList<>();
+//            BankingRelation bankingRelation = null;
 
-            List<BankingRelation> bankingRelations = new ArrayList<>();
-            BankingRelation bankingRelation = null;
-
-            if(!CommonUtils.isObjectNullOrEmpty(plRetailApplicantRequest) && !CommonUtils.isObjectNullOrEmpty(plRetailApplicantRequest.getBankingRelationshipList()))
-            {
-                for(BankRelationshipRequest bankRelationshipRequest : plRetailApplicantRequest.getBankingRelationshipList()) {
-                    bankingRelation = new BankingRelation();
-                    BeanUtils.copyProperties(bankRelationshipRequest, bankingRelation);
-                    bankingRelation.setApplicationId(plRetailApplicantRequest.getApplicationId());
-                    if(bankRelationshipRequest.getId() == null) {
-                        bankingRelation.setCreatedBy(userId);
-                        bankingRelation.setCreatedDate(new Date());
-                        bankingRelation.setIsActive(true);
-                    }
-                    bankingRelation.setModifiedBy(userId);
-                    bankingRelation.setModifiedDate(new Date());
-                    bankingRelations.add(bankingRelation);
-                }
-            }
-
-            bankingRelationlRepository.save(bankingRelations);
+//            if(!CommonUtils.isObjectNullOrEmpty(plRetailApplicantRequest) && !CommonUtils.isObjectNullOrEmpty(plRetailApplicantRequest.getBankingRelationshipList()))
+//            {
+//                for(BankRelationshipRequest bankRelationshipRequest : plRetailApplicantRequest.getBankingRelationshipList()) {
+//                    bankingRelation = new BankingRelation();
+//                    BeanUtils.copyProperties(bankRelationshipRequest, bankingRelation);
+//                    bankingRelation.setApplicationId(plRetailApplicantRequest.getApplicationId());
+//                    if(bankRelationshipRequest.getId() == null) {
+//                        bankingRelation.setCreatedBy(userId);
+//                        bankingRelation.setCreatedDate(new Date());
+//                        bankingRelation.setIsActive(true);
+//                    }
+//                    bankingRelation.setModifiedBy(userId);
+//                    bankingRelation.setModifiedDate(new Date());
+//                    bankingRelations.add(bankingRelation);
+//                }
+//            }
+//
+//            bankingRelationlRepository.save(bankingRelations);
 
             applicantRepository.save(applicantDetail);
 
