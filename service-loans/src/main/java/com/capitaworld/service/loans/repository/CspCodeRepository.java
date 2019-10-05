9a9f5b76a568c7e38e329858d8364253ff534dcd
akshay <akshay.patel@capitaworld.com>
2019-10-05 16:16:45 +0530

    Added nativ query flag

--- service-loans/src/main/java/com/capitaworld/service/loans/repository/CspCodeRepository.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/repository/CspCodeRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface CspCodeRepository extends JpaRepository<CspCode,Long> {
 	
-	@Query(value="SELECT count(cs.id) from loan_application.csp_code cs where LOWER(loan_application.fn_remove_special_chars(cs.description)) =:code and cs.org_id =:orgId")
+	@Query(value="SELECT count(cs.id) from loan_application.csp_code cs where LOWER(loan_application.fn_remove_special_chars(cs.description)) =:code and cs.org_id =:orgId",nativeQuery = true)
 	public Long isCodeExists(@Param("code")String code,@Param("orgId")Long orgId);
 }
