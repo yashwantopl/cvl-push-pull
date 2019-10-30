21713029607d551ca18856e2abf92996f78a56a9
DhavalCW <dhaval@capitaworld.com>
2019-10-30 19:45:13 +0530

    update

--- service-loans/src/main/java/com/capitaworld/service/loans/repository/colending/RecommendDetailRepository.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/repository/colending/RecommendDetailRepository.java
@@ -2,11 +2,15 @@ package com.capitaworld.service.loans.repository.colending;
 
 import com.capitaworld.service.loans.domain.colending.RecommendDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Created by dhaval.panchal on 12-Sep-19.
  */
 
 public interface RecommendDetailRepository extends JpaRepository<RecommendDetail,Long>{
-    RecommendDetail getOneByApplicationId(Long applicationId);
+
+    @Query(value = "select * from recommend_detail rd where rd.application_id =:applicationId order by id desc limit 1",nativeQuery = true)
+    public RecommendDetail getByApplicationIdOrderByIdDescLimit1(@Param("applicationId") Long applicationId);
 }
