929bb9d5de5b4ebe3ea6ba9e03b963e3fc73a64f
DhavalCW <dhaval@capitaworld.com>
2019-10-31 11:30:51 +0530

    code place changed

--- service-loans/src/main/java/com/capitaworld/service/loans/service/teaser/primaryview/impl/CorporatePrimaryViewServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/teaser/primaryview/impl/CorporatePrimaryViewServiceImpl.java
@@ -12,7 +12,9 @@ import java.util.List;
 import java.util.Map;
 
 import com.capitaworld.service.loans.domain.colending.RecommendDetail;
+import com.capitaworld.service.loans.domain.sanction.LoanSanctionDomain;
 import com.capitaworld.service.loans.repository.colending.RecommendDetailRepository;
+import com.capitaworld.service.loans.repository.sanction.LoanSanctionRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
@@ -237,7 +239,9 @@ public class CorporatePrimaryViewServiceImpl implements CorporatePrimaryViewServ
 
 	@Autowired
 	private RecommendDetailRepository recommendDetailRepository;
-    
+
+	@Autowired
+    private LoanSanctionRepository loanSanctionRepository;
 	
 	DecimalFormat decim = new DecimalFormat("#,###.00");
 
@@ -254,16 +258,6 @@ public class CorporatePrimaryViewServiceImpl implements CorporatePrimaryViewServ
 			corporatePrimaryViewResponse.setNbfcData(nbfcData);
 			ProposalDetails proposalDetailsForBank = proposalDetailsRepository.findFirstByApplicationIdAndIsActiveAndNbfcFlowOrderByIdDesc(applicationId, true, 2);
 			proposalId = proposalDetailsForBank.getId();
-
-			RecommendDetail detail = recommendDetailRepository.getByApplicationIdOrderByIdDescLimit1(applicationId);
-			if (!CommonUtils.isObjectNullOrEmpty(detail)) {
-				nbfcData.setRecommendedValue(detail.getValue());
-				nbfcData.setRecommendedTenure(detail.getTenure());
-				nbfcData.setRecommendedRoi(detail.getRoi());
-				nbfcData.setRecommendedProcessingFee(detail.getProcessingFee());
-				nbfcData.setRecommendedRemark(detail.getRemark());
-			}
-
 		}
 	// END
 		
@@ -1493,6 +1487,16 @@ public class CorporatePrimaryViewServiceImpl implements CorporatePrimaryViewServ
 				corporatePrimaryView.setBankEmiAmount(proposalDetailsForBank.getEmi() != 0 ? CommonUtils.convertValueWithoutDecimal(proposalDetailsForBank.getEmi().doubleValue()) : "0");
 				corporatePrimaryView.setBankProcessingFees(proposalDetailsForBank.getProcessingFee() );
 			}
+
+			RecommendDetail detail = recommendDetailRepository.getByApplicationIdOrderByIdDescLimit1(applicationId);
+			if (!CommonUtils.isObjectNullOrEmpty(detail)) {
+				corporatePrimaryView.setRecommendedValue(detail.getValue());
+				corporatePrimaryView.setRecommendedTenure(detail.getTenure());
+				corporatePrimaryView.setRecommendedRoi(detail.getRoi());
+				corporatePrimaryView.setRecommendedProcessingFee(detail.getProcessingFee());
+				corporatePrimaryView.setRecommendedRemark(detail.getRemark());
+			}
+
 			return corporatePrimaryView;
 	}
 //	ENDS HERE CO-ORIGIN CODE 
