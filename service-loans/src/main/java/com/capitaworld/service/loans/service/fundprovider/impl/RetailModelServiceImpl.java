1e6c9e8ed462e1053e231e08a930cadeb45f383e
jitesh.savaliya <jitesh.savaliya@CWDT011.CORP.CAPITAWORLD.COM>
2019-11-26 15:35:23 +0530

    unused code

--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundprovider/impl/RetailModelServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundprovider/impl/RetailModelServiceImpl.java
@@ -10,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -56,15 +55,8 @@ public class RetailModelServiceImpl implements RetailModelService {
 	@Autowired
 	private WorkflowClient workflowClient;
 	
+	@Autowired
 	private FPAsyncComponent fpAsyncComp;
-	
-	@Lazy
-	RetailModelServiceImpl(FPAsyncComponent fpAsyncComp)
-	{
-
-		logger.info("init @Lazy FPAsyncComponent");
-		this.fpAsyncComp=fpAsyncComp;
-	}
 
 	@Override
 	public Boolean save(RetailModelRequest modelRequest) {
