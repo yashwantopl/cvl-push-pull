a685077b56bbca33b050b1922bb04dd195215535
Rohit Chaudhary <rohit.chaudhary@onlinepsbloans.com>
2020-04-02 10:55:50 +0530

    -- log added.

--- service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/impl/FundSeekerInputRequestServiceImpl.java
+++ service-loans/src/main/java/com/capitaworld/service/loans/service/fundseeker/corporate/impl/FundSeekerInputRequestServiceImpl.java
@@ -667,9 +667,10 @@ public class FundSeekerInputRequestServiceImpl implements FundSeekerInputRequest
 	        gstr1Request.setApplicationId(fsInputReq.getApplicationId());
 	        gstr1Request.setGstin(fsInputReq.getGstIn());
 			
+	        logger.info("gstClient *******************------->" + MultipleJSONObjectHelper.getStringfromObject(gstClient));
+	        
 			GstResponse calculationForScoring = gstClient.getCalculations(gstr1Request);
 			
-			logger.info("gstClient *******************------->" + MultipleJSONObjectHelper.getStringfromObject(gstClient));
 			
             if(!CommonUtils.isObjectNullOrEmpty(calculationForScoring) && !CommonUtils.isObjectNullOrEmpty(calculationForScoring.getData())){
                 GstCalculation gstCalculation = MultipleJSONObjectHelper.getObjectFromMap((LinkedHashMap<String,Object>)calculationForScoring.getData(),GstCalculation.class);
