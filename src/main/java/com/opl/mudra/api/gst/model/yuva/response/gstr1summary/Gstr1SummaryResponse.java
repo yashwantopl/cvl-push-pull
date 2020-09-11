a7e3533f2e8995c3f89b193abf7dee8ca43c3ff7
rahul.gupta <rahul.gupta@CWDT014.corp.capitaworld.com>
2020-09-11 18:45:43 +0530

    -- Solved issue.

--- src/main/java/com/opl/mudra/api/gst/model/yuva/response/gstr1summary/Gstr1SummaryResponse.java
+++ src/main/java/com/opl/mudra/api/gst/model/yuva/response/gstr1summary/Gstr1SummaryResponse.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.opl.mudra.api.gst.model.yuva.response.authentication.Error;
+
 
 /**
  * @author nilay
