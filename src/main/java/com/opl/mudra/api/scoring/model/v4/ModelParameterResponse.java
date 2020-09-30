eb2d629b0f33acfdcea1a88a9cc41b2dac88a0b5
akshay <akshay.patel@capitaworld.com>
2020-09-30 17:43:38 +0530

    Changes for Scoring reponse classes

--- src/main/java/com/opl/mudra/api/scoring/model/v4/ModelParameterResponse.java
+++ src/main/java/com/opl/mudra/api/scoring/model/v4/ModelParameterResponse.java
@@ -3,6 +3,13 @@ package com.opl.mudra.api.scoring.model.v4;
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ModelParameterResponse {
 
 	private Long id;
@@ -10,80 +17,18 @@ public class ModelParameterResponse {
 	private Long riskMakerId;
 	private Double maxScore;
 	private Integer yearId;
+	private String name;
 	private Boolean isYearDisplay;
-	private List<ModelParameterOptionRequest> modelParameterOptions=new ArrayList<ModelParameterOptionRequest>();
+	private String addivalues;
+	private List<ModelParameterOptionRequest> modelParameterOptions = new ArrayList<ModelParameterOptionRequest>();
 	private List<FieldMasterRequest> FieldMasterRequestList;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getFieldMasterId() {
-		return fieldMasterId;
-	}
-
-	public void setFieldMasterId(Long fieldMasterId) {
+	public ModelParameterResponse(Long fieldMasterId, String name, Integer yearId, Double maxScore, String addivalues) {
+		super();
 		this.fieldMasterId = fieldMasterId;
-	}
-
-	public Long getRiskMakerId() {
-		return riskMakerId;
-	}
-
-	public void setRiskMakerId(Long riskMakerId) {
-		this.riskMakerId = riskMakerId;
-	}
-
-	public Double getMaxScore() {
-		return maxScore;
-	}
-
-	public void setMaxScore(Double maxScore) {
-		this.maxScore = maxScore;
-	}
-
-	public List<ModelParameterOptionRequest> getModelParameterOptions() {
-		return modelParameterOptions;
-	}
-
-	public void setModelParameterOptions(List<ModelParameterOptionRequest> modelParameterOptions) {
-		this.modelParameterOptions = modelParameterOptions;
-	}
-
-	public Integer getYearId() {
-		return yearId;
-	}
-
-	public void setYearId(Integer yearId) {
+		this.name = name;
 		this.yearId = yearId;
+		this.maxScore = maxScore;
+		this.addivalues = addivalues;
 	}
-
-	public List<FieldMasterRequest> getFieldMasterRequestList() {
-		return FieldMasterRequestList;
-	}
-
-	public void setFieldMasterRequestList(List<FieldMasterRequest> fieldMasterRequestList) {
-		FieldMasterRequestList = fieldMasterRequestList;
-	}
-
-	public Boolean getYearDisplay() {
-		return isYearDisplay;
-	}
-
-	public void setYearDisplay(Boolean isYearDisplay) {
-		this.isYearDisplay = isYearDisplay;
-	}
-
-	@Override
-	public String toString() {
-		return "ModelParameterResponse [id=" + id + ", fieldMasterId=" + fieldMasterId + ", riskMakerId=" + riskMakerId
-				+ ", maxScore=" + maxScore + ", yearId=" + yearId + ", isYearDisplay=" + isYearDisplay
-				+ ", modelParameterOptions=" + modelParameterOptions + ", FieldMasterRequestList="
-				+ FieldMasterRequestList + "]";
-	}
-
 }
