d099f3b5555272ae34d3d3c8e8b6baf94ab61a89
rahul.gupta <rahul.gupta@CWDT014.corp.capitaworld.com>
2020-10-26 12:22:33 +0530

    -- Added bank bureau response

--- src/main/java/com/opl/mudra/api/matchengine/model/BankBureauRequest.java
+++ src/main/java/com/opl/mudra/api/matchengine/model/BankBureauRequest.java
@@ -85,6 +85,21 @@ public class BankBureauRequest implements Serializable{
     
     private String description;
     
+    private Boolean dpdMatch6Month;
+    private String dpd6MonthFlag;
+    private Double dpd6Month;
+    
+    private String dpd12MonthFlagIndividual;
+    private String dpd12MonthIndividual;
+    private String dpd6MonthFlagIndividual;
+    private String dpd6MonthIndividual;
+    private String dpd3MonthFlagIndividual;
+    private String dpd3MonthIndividual;
+    
+    private Boolean dpdMatch12MonthIndividual;
+    private Boolean dpdMatch6MonthIndividual;
+    private Boolean dpdMatch3MonthIndividual;
+    
 	public Long getId() {
 		return id;
 	}
@@ -395,5 +410,101 @@ public class BankBureauRequest implements Serializable{
 
 	public void setLoansFlag(String loansFlag) {
 		this.loansFlag = loansFlag;
+	}
+
+	public Boolean getDpdMatch6Month() {
+		return dpdMatch6Month;
+	}
+
+	public void setDpdMatch6Month(Boolean dpdMatch6Month) {
+		this.dpdMatch6Month = dpdMatch6Month;
+	}
+
+	public String getDpd12MonthFlagIndividual() {
+		return dpd12MonthFlagIndividual;
+	}
+
+	public void setDpd12MonthFlagIndividual(String dpd12MonthFlagIndividual) {
+		this.dpd12MonthFlagIndividual = dpd12MonthFlagIndividual;
+	}
+
+	public String getDpd12MonthIndividual() {
+		return dpd12MonthIndividual;
+	}
+
+	public void setDpd12MonthIndividual(String dpd12MonthIndividual) {
+		this.dpd12MonthIndividual = dpd12MonthIndividual;
+	}
+
+	public String getDpd6MonthFlagIndividual() {
+		return dpd6MonthFlagIndividual;
+	}
+
+	public void setDpd6MonthFlagIndividual(String dpd6MonthFlagIndividual) {
+		this.dpd6MonthFlagIndividual = dpd6MonthFlagIndividual;
+	}
+
+	public String getDpd6MonthIndividual() {
+		return dpd6MonthIndividual;
+	}
+
+	public void setDpd6MonthIndividual(String dpd6MonthIndividual) {
+		this.dpd6MonthIndividual = dpd6MonthIndividual;
+	}
+
+	public String getDpd3MonthFlagIndividual() {
+		return dpd3MonthFlagIndividual;
+	}
+
+	public void setDpd3MonthFlagIndividual(String dpd3MonthFlagIndividual) {
+		this.dpd3MonthFlagIndividual = dpd3MonthFlagIndividual;
+	}
+
+	public String getDpd3MonthIndividual() {
+		return dpd3MonthIndividual;
+	}
+
+	public void setDpd3MonthIndividual(String dpd3MonthIndividual) {
+		this.dpd3MonthIndividual = dpd3MonthIndividual;
+	}
+
+	public Boolean getDpdMatch12MonthIndividual() {
+		return dpdMatch12MonthIndividual;
+	}
+
+	public void setDpdMatch12MonthIndividual(Boolean dpdMatch12MonthIndividual) {
+		this.dpdMatch12MonthIndividual = dpdMatch12MonthIndividual;
+	}
+
+	public Boolean getDpdMatch6MonthIndividual() {
+		return dpdMatch6MonthIndividual;
+	}
+
+	public void setDpdMatch6MonthIndividual(Boolean dpdMatch6MonthIndividual) {
+		this.dpdMatch6MonthIndividual = dpdMatch6MonthIndividual;
+	}
+
+	public Boolean getDpdMatch3MonthIndividual() {
+		return dpdMatch3MonthIndividual;
+	}
+
+	public void setDpdMatch3MonthIndividual(Boolean dpdMatch3MonthIndividual) {
+		this.dpdMatch3MonthIndividual = dpdMatch3MonthIndividual;
+	}
+
+	public String getDpd6MonthFlag() {
+		return dpd6MonthFlag;
+	}
+
+	public void setDpd6MonthFlag(String dpd6MonthFlag) {
+		this.dpd6MonthFlag = dpd6MonthFlag;
+	}
+
+	public Double getDpd6Month() {
+		return dpd6Month;
+	}
+
+	public void setDpd6Month(Double dpd6Month) {
+		this.dpd6Month = dpd6Month;
 	}
 }
