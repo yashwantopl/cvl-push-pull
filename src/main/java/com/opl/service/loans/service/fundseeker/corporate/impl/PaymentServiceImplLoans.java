db4062d64324b625d95446db5ac90bd97d376ca0
akash.jain <akash.jain@CWDT045.corp.capitaworld.com>
2020-11-05 11:49:37 +0530

    change educational status enum

--- src/main/java/com/opl/service/loans/service/fundseeker/corporate/impl/PaymentServiceImplLoans.java
+++ src/main/java/com/opl/service/loans/service/fundseeker/corporate/impl/PaymentServiceImplLoans.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -102,8 +103,12 @@ public class PaymentServiceImplLoans implements PaymentServiceLoans{
 	@Autowired
 	UsersClient userClient;
 	
-	@Autowired
 	AsyncComponent asyncComp;
+	
+	@Lazy
+	PaymentServiceImplLoans(AsyncComponent asyncComp){
+		this.asyncComp = asyncComp;
+	}
 	
 	@Autowired
 	GatewayClient gatewayClient;
