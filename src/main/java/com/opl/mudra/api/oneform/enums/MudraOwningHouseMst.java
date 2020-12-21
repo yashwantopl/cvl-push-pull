cd8ac649bb8d964622a9e2133cc79b399f72de4c
jay.barbhaya <jay.barbhaya@onlinepsbloans.com>
2020-12-21 17:58:05 +0530

    -- change enums

--- src/main/java/com/opl/mudra/api/oneform/enums/MudraOwningHouseMst.java
+++ src/main/java/com/opl/mudra/api/oneform/enums/MudraOwningHouseMst.java
@@ -2,15 +2,15 @@ package com.opl.mudra.api.oneform.enums;
 
 public enum MudraOwningHouseMst {
 
-	RENTED_FOR_MORE_THAN_3_YEAR(1,"Rented for more than 3 year","Rented for more than 3 year"),
-	RENTED_FOR_UPTO_3_YEARS(2,"Rented for upto 3 years","Rented for upto 3 years "),
-	OWING_A_HOUSE_NOT_MORTGAGED_SAME_PLACE(3,"Owing a house - Not Mortgaged - Same Place","Owing a house - Not Mortgaged - Same Place"),
-	OWNING_A_HOUSE_NOT_MORTGAGED_OTHER_PLACE(4,"Owning a house - Not Mortgaged - Other Place","Owning a house - Not Mortgaged - Other Place"),
-	OWING_A_HOUSE_MORTGAGED_IN_THE_SAME_PLACE(5,"Owing a house - Mortgaged - In the same place","Owing a house - Mortgaged - In the same place"),
-	OWING_A_HOUSE_MORTGAGED_AT_OTHER_PLACE(6,"Owing a house - Mortgaged - At other place","Owing a house - Mortgaged - At other place"),
-	HOUSE_OWNED_BY_SPOUSE(7,"House owned by spouse","House owned by spouse"),
-	LIVING_WITH_FAMILY(8,"Living with Family","Living with Family"),
-	OTHERS(9,"Others","Others");
+	HOME_OWNER(1,"Home Owner","Home Owner"),
+	OWNED_BY_BLOOD_RELATIVE(2,"Owned by Blood Relative","Owned by Blood Relative"),
+	OWNED_BY_SPOUSE(3,"Owned by Spouse","Owned by Spouse"),
+	LIVING_IN_FAMILY(4,"Living in Family","Living in Family"),
+	TENANT_ACCOMODATION_GIVEN_BY_RESIDENCE(5,"Tenant (Accomodation given by residence) - Range","Tenant (Accomodation given by residence) - Range"),
+	TENANT_ACCOMODATION_GIVEN_BY_PSU_AND_GOVT(6,"Tenant (Accomodation given by PSU and Govt)","Tenant (Accomodation given by PSU and Govt)"),
+//	HOUSE_OWNED_BY_SPOUSE(7,"House owned by spouse","House owned by spouse"),
+//	LIVING_WITH_FAMILY(8,"Living with Family","Living with Family"),
+	OTHERS(7,"Others","Others");
 
     private final Integer id;
     private final String value;
@@ -37,22 +37,18 @@ public enum MudraOwningHouseMst {
     public static MudraOwningHouseMst getById(Integer id) {
         switch (id) {
             case 1:
-                return RENTED_FOR_MORE_THAN_3_YEAR;
+                return HOME_OWNER;
             case 2:
-                return RENTED_FOR_UPTO_3_YEARS;
+                return OWNED_BY_BLOOD_RELATIVE;
             case 3:
-            	return OWING_A_HOUSE_NOT_MORTGAGED_SAME_PLACE;
+            	return OWNED_BY_SPOUSE;
             case 4:
-            	return OWNING_A_HOUSE_NOT_MORTGAGED_OTHER_PLACE;
+            	return LIVING_IN_FAMILY;
             case 5:
-            	return OWING_A_HOUSE_MORTGAGED_IN_THE_SAME_PLACE;
+            	return TENANT_ACCOMODATION_GIVEN_BY_RESIDENCE;
             case 6:
-            	return OWING_A_HOUSE_MORTGAGED_AT_OTHER_PLACE;
+            	return TENANT_ACCOMODATION_GIVEN_BY_PSU_AND_GOVT;
             case 7:
-            	return HOUSE_OWNED_BY_SPOUSE;
-            case 8:
-            	return LIVING_WITH_FAMILY;
-            case 9:
             	return OTHERS;
             default:
                 return null;
