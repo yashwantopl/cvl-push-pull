042f5ef4dc5ca6486e0cfc210fa466bca4c5eb96
Harsukh Ghumaliya <harsukh.ghumaliya@onlinepsbloans.com>
2020-12-22 12:02:18 +0530

    -updates

--- src/main/java/com/opl/mudra/api/scoring/model/GenericCheckerReqRes.java
+++ src/main/java/com/opl/mudra/api/scoring/model/GenericCheckerReqRes.java
@@ -23,10 +23,85 @@ public class GenericCheckerReqRes implements Serializable  {
     private Long currentStep;
     private Long toStep;
     private Long actionId;
-    private Long userId;
     private String code;
-    private String remark;
     private Boolean actionFlag;
     private Boolean isEdit;
+    private String remark;
     private Integer statusId;
+    private Long userId;
+    private Long businessTypeId;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getJobId() {
+		return jobId;
+	}
+	public void setJobId(Long jobId) {
+		this.jobId = jobId;
+	}
+	public Long getCurrentStep() {
+		return currentStep;
+	}
+	public void setCurrentStep(Long currentStep) {
+		this.currentStep = currentStep;
+	}
+	public Long getToStep() {
+		return toStep;
+	}
+	public void setToStep(Long toStep) {
+		this.toStep = toStep;
+	}
+	public Long getActionId() {
+		return actionId;
+	}
+	public void setActionId(Long actionId) {
+		this.actionId = actionId;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public Boolean getActionFlag() {
+		return actionFlag;
+	}
+	public void setActionFlag(Boolean actionFlag) {
+		this.actionFlag = actionFlag;
+	}
+	public Boolean getIsEdit() {
+		return isEdit;
+	}
+	public void setIsEdit(Boolean isEdit) {
+		this.isEdit = isEdit;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	public Integer getStatusId() {
+		return statusId;
+	}
+	public void setStatusId(Integer statusId) {
+		this.statusId = statusId;
+	}
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public Long getBusinessTypeId() {
+		return businessTypeId;
+	}
+	public void setBusinessTypeId(Long businessTypeId) {
+		this.businessTypeId = businessTypeId;
+	}
+    
+    
 }
